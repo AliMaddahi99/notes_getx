6cd9fdac62ff7223460dfecce19f69a3f3f7b750
Ali Maddahi <alimaddahi99@gmail.com>
2022-07-06 19:54:54 +0430

    fab onPress navigates to AddNote

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:notes_getx/screens/add_note.dart';
 
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
@@ -10,7 +12,9 @@ class Home extends StatelessWidget {
         title: const Text("Notes Getx"),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () => {},
+        onPressed: () => {
+          Get.to(AddNote()),
+        },
         child: const Icon(Icons.add),
       ),
     );
