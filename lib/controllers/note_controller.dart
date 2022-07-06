8d8fb5793e2c809ba2bdcc88622db1e72e94a26c
Ali Maddahi <alimaddahi99@gmail.com>
2022-07-06 19:04:12 +0430

    create note_controller

---
+++ lib/controllers/note_controller.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+import 'package:get/get.dart';
+import 'package:notes_getx/models/note_model.dart';
+
+class NoteController extends GetxController {
+  final TextEditingController titleTextController = TextEditingController();
+  final TextEditingController noteTextController = TextEditingController();
+  final FocusNode noteFocusNode = FocusNode();
+
+  var notes = <NoteModel>[].obs;
+}
