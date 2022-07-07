19742370d0bd653f3789c7da7238a11bb0f9861b
Ali Maddahi <alimaddahi99@gmail.com>
2022-07-07 21:55:34 +0430

    create gridview_note_card

---
+++ lib/widgets/gridview_note_card.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:notes_getx/controllers/note_controller.dart';
+
+class GridViewNoteCard extends StatelessWidget {
+  GridViewNoteCard({Key? key}) : super(key: key);
+
+  final NoteController controller = Get.find();
+
+  @override
+  Widget build(BuildContext context) {
+    return Obx(
+      () => GridView.builder(
+        padding: const EdgeInsets.all(8.0),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+        ),
+        itemBuilder: (context, index) {
+          return Card(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(8.0),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  controller.notes[index].title!.isNotEmpty
+                      ? Padding(
+                          padding: const EdgeInsets.only(bottom: 8.0),
+                          child: Text(
+                            controller.notes[index].title!,
+                            maxLines: 1,
+                            overflow: TextOverflow.ellipsis,
+                            style: const TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                        )
+                      : const SizedBox.shrink(),
+                  controller.notes[index].note.isNotEmpty
+                      ? Expanded(
+                          child: Text(
+                            controller.notes[index].note,
+                            maxLines: 4,
+                            overflow: TextOverflow.ellipsis,
+                            style: const TextStyle(
+                              color: Colors.black54,
+                            ),
+                          ),
+                        )
+                      : const SizedBox.shrink(),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 16.0),
+                    child: Text(
+                      controller.notes[index].dateTime.toString(),
+                      style: const TextStyle(
+                          fontSize: 10.0, color: Colors.black45),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
+        itemCount: controller.notes.length,
+      ),
+    );
+  }
+}
