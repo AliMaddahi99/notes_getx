0302b64728fa6c6bbda50157c32167f0082b32c8
Ali Maddahi <alimaddahi99@gmail.com>
2022-07-07 18:02:47 +0430

    implement addNote() in note_controller

--- lib/controllers/note_controller.dart
+++ lib/controllers/note_controller.dart
@@ -8,6 +8,28 @@ class NoteController extends GetxController {
   final FocusNode noteFocusNode = FocusNode();
 
   var notes = <NoteModel>[].obs;
+  int _noteId = 0;
+
+  void addNote({String? title, required String note}) {
+    titleTextController.clear();
+    noteTextController.clear();
+
+    if ((title == null || title == "") && note.isEmpty) {
+      Get.snackbar(
+        "Empty title and note!",
+        "Please at least write a title or note",
+        snackPosition: SnackPosition.BOTTOM,
+      );
+    } else {
+      notes.insert(
+          0,
+          NoteModel(
+              id: _noteId, note: note, title: title, dateTime: DateTime.now()));
+      _noteId++;
+    }
+
+    noteFocusNode.requestFocus();
+  }
 
   @override
   void onInit() {
