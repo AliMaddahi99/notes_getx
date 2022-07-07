a0ebeaa5d3a7f290eaf4716d8623b0cdb16e32f1
Ali Maddahi <alimaddahi99@gmail.com>
2022-07-08 00:33:43 +0430

    add gesture detection to listview and gridview

--- lib/widgets/gridview_note_card.dart
+++ lib/widgets/gridview_note_card.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:notes_getx/controllers/note_controller.dart';
+import 'package:notes_getx/screens/edit_todo.dart';
 
 class GridViewNoteCard extends StatelessWidget {
   GridViewNoteCard({Key? key}) : super(key: key);
@@ -16,48 +17,81 @@ class GridViewNoteCard extends StatelessWidget {
           crossAxisCount: 2,
         ),
         itemBuilder: (context, index) {
-          return Card(
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(8.0),
-            ),
-            child: Padding(
-              padding: const EdgeInsets.all(12.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  controller.notes[index].title!.isNotEmpty
-                      ? Padding(
-                          padding: const EdgeInsets.only(bottom: 8.0),
-                          child: Text(
-                            controller.notes[index].title!,
-                            maxLines: 1,
-                            overflow: TextOverflow.ellipsis,
-                            style: const TextStyle(fontWeight: FontWeight.bold),
-                          ),
-                        )
-                      : const SizedBox.shrink(),
-                  controller.notes[index].note.isNotEmpty
-                      ? Expanded(
-                          child: Text(
-                            controller.notes[index].note,
-                            maxLines: 4,
-                            overflow: TextOverflow.ellipsis,
-                            style: const TextStyle(
-                              color: Colors.black54,
-                            ),
-                          ),
-                        )
-                      : const SizedBox.shrink(),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 16.0),
-                    child: Text(
-                      controller.notes[index].dateTime.toString(),
-                      style: const TextStyle(
-                          fontSize: 10.0, color: Colors.black45),
-                    ),
+          return MouseRegion(
+            cursor: SystemMouseCursors.click,
+            child: GestureDetector(
+              onTap: () {
+                Get.to(() => EditNote(), arguments: index);
+              },
+              onLongPress: () {
+                Get.dialog(AlertDialog(
+                    title: const Text("Delete"),
+                    content: const Text("Do you want delete this note?"),
+                    actions: [
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          primary: Colors.red,
+                          onPrimary: Colors.white,
+                        ),
+                        onPressed: () {
+                          controller.notes.removeAt(index);
+                          Get.back();
+                        },
+                        child: const Text("Delete"),
+                      ),
+                      ElevatedButton(
+                        onPressed: () {
+                          Get.back();
+                        },
+                        child: const Text("Cancel"),
+                      ),
+                    ]));
+              },
+              child: Card(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      controller.notes[index].title!.isNotEmpty
+                          ? Padding(
+                              padding: const EdgeInsets.only(bottom: 8.0),
+                              child: Text(
+                                controller.notes[index].title!,
+                                maxLines: 1,
+                                overflow: TextOverflow.ellipsis,
+                                style: const TextStyle(
+                                    fontWeight: FontWeight.bold),
+                              ),
+                            )
+                          : const SizedBox.shrink(),
+                      controller.notes[index].note.isNotEmpty
+                          ? Expanded(
+                              child: Text(
+                                controller.notes[index].note,
+                                maxLines: 4,
+                                overflow: TextOverflow.ellipsis,
+                                style: const TextStyle(
+                                  color: Colors.black54,
+                                ),
+                              ),
+                            )
+                          : const SizedBox.shrink(),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 16.0),
+                        child: Text(
+                          controller.notes[index].dateTime.toString(),
+                          style: const TextStyle(
+                              fontSize: 10.0, color: Colors.black45),
+                        ),
+                      ),
+                    ],
                   ),
-                ],
+                ),
               ),
             ),
           );
