19900536050b925f80278793a236ec902ab16553
Ali Maddahi <alimaddahi99@gmail.com>
2022-07-08 00:47:30 +0430

    GetStorage.init() in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:get_storage/get_storage.dart';
 
 import 'notes_app.dart';
 
 void main() {
+  GetStorage.init();
   runApp(const NotesApp());
 }
