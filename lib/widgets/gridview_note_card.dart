fbccd7fd293abce4710506fc009f36f387829540
Ali Maddahi <alimaddahi99@gmail.com>
2022-07-10 21:26:31 +0430

    remove MouseRegion and GestureDetector, and implement InkWell as Card child in gridview_note_card

--- lib/widgets/gridview_note_card.dart
+++ lib/widgets/gridview_note_card.dart
@@ -28,71 +28,70 @@ class GridViewNoteCard extends StatelessWidget {
                     duration: const Duration(milliseconds: 300),
                     child: ScaleAnimation(
                       child: FadeInAnimation(
-                        child: MouseRegion(
-                          cursor: SystemMouseCursors.click,
-                          child: GestureDetector(
+                        child: Card(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(16.0),
+                          ),
+                          child: InkWell(
+                            mouseCursor: SystemMouseCursors.click,
+                            borderRadius: BorderRadius.circular(16.0),
                             onTap: () {
                               Get.to(() => EditNote(), arguments: index);
                             },
                             onLongPress: () {
                               Get.dialog(DeleteDialog(index: index));
                             },
-                            child: Card(
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(16.0),
-                              ),
-                              child: Padding(
-                                padding: const EdgeInsets.all(16.0),
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    controller.notes[index].title!.isNotEmpty
-                                        ? Padding(
-                                            padding: const EdgeInsets.only(
-                                              bottom: 8.0,
+                            child: Padding(
+                              padding: const EdgeInsets.all(16.0),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  controller.notes[index].title!.isNotEmpty
+                                      ? Padding(
+                                          padding: const EdgeInsets.only(
+                                            bottom: 8.0,
+                                          ),
+                                          child: Text(
+                                            controller.notes[index].title!,
+                                            maxLines: 1,
+                                            overflow: TextOverflow.ellipsis,
+                                            style: const TextStyle(
+                                              fontSize: 18.0,
+                                              fontWeight: FontWeight.bold,
                                             ),
-                                            child: Text(
-                                              controller.notes[index].title!,
-                                              maxLines: 1,
-                                              overflow: TextOverflow.ellipsis,
-                                              style: const TextStyle(
-                                                fontSize: 18.0,
-                                                fontWeight: FontWeight.bold,
-                                              ),
+                                          ),
+                                        )
+                                      : const SizedBox.shrink(),
+                                  controller.notes[index].note.isNotEmpty
+                                      ? Padding(
+                                          padding: const EdgeInsets.only(
+                                            bottom: 8.0,
+                                          ),
+                                          child: Text(
+                                            controller.notes[index].note,
+                                            maxLines: 4,
+                                            overflow: TextOverflow.ellipsis,
+                                            style: const TextStyle(
+                                              fontSize: 16.0,
+                                              color: Colors.black54,
                                             ),
-                                          )
-                                        : const SizedBox.shrink(),
-                                    controller.notes[index].note.isNotEmpty
-                                        ? Padding(
-                                            padding: const EdgeInsets.only(
-                                              bottom: 8.0,
-                                            ),
-                                            child: Text(
-                                              controller.notes[index].note,
-                                              maxLines: 4,
-                                              overflow: TextOverflow.ellipsis,
-                                              style: const TextStyle(
-                                                fontSize: 16.0,
-                                                color: Colors.black54,
-                                              ),
-                                            ),
-                                          )
-                                        : const SizedBox.shrink(),
-                                    Padding(
-                                      padding: const EdgeInsets.only(
-                                        top: 8.0,
-                                      ),
-                                      child: Text(
-                                        controller.notes[index].dateTime
-                                            .toString(),
-                                        style: const TextStyle(
-                                          fontSize: 12.0,
-                                          color: Colors.black45,
-                                        ),
+                                          ),
+                                        )
+                                      : const SizedBox.shrink(),
+                                  Padding(
+                                    padding: const EdgeInsets.only(
+                                      top: 8.0,
+                                    ),
+                                    child: Text(
+                                      controller.notes[index].dateTime
+                                          .toString(),
+                                      style: const TextStyle(
+                                        fontSize: 12.0,
+                                        color: Colors.black45,
                                       ),
                                     ),
-                                  ],
-                                ),
+                                  ),
+                                ],
                               ),
                             ),
                           ),
