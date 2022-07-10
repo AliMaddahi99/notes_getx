3e971c3287a10fcf92956c3e9cdd40dc18d5cfe9
Ali Maddahi <alimaddahi99@gmail.com>
2022-07-10 21:55:51 +0430

    replace Card with NoteCard

--- lib/widgets/gridview_note_card.dart
+++ lib/widgets/gridview_note_card.dart
@@ -3,8 +3,7 @@ import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:get/get.dart';
 import 'package:notes_getx/controllers/note_controller.dart';
-import 'package:notes_getx/screens/edit_note.dart';
-import 'package:notes_getx/widgets/delete_dialog.dart';
+import 'package:notes_getx/widgets/note_card.dart';
 
 class GridViewNoteCard extends StatelessWidget {
   GridViewNoteCard({Key? key}) : super(key: key);
@@ -28,74 +27,7 @@ class GridViewNoteCard extends StatelessWidget {
                     duration: const Duration(milliseconds: 300),
                     child: ScaleAnimation(
                       child: FadeInAnimation(
-                        child: Card(
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(16.0),
-                          ),
-                          child: InkWell(
-                            mouseCursor: SystemMouseCursors.click,
-                            borderRadius: BorderRadius.circular(16.0),
-                            onTap: () {
-                              Get.to(() => EditNote(), arguments: index);
-                            },
-                            onLongPress: () {
-                              Get.dialog(DeleteDialog(index: index));
-                            },
-                            child: Padding(
-                              padding: const EdgeInsets.all(16.0),
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  controller.notes[index].title!.isNotEmpty
-                                      ? Padding(
-                                          padding: const EdgeInsets.only(
-                                            bottom: 8.0,
-                                          ),
-                                          child: Text(
-                                            controller.notes[index].title!,
-                                            maxLines: 1,
-                                            overflow: TextOverflow.ellipsis,
-                                            style: const TextStyle(
-                                              fontSize: 18.0,
-                                              fontWeight: FontWeight.bold,
-                                            ),
-                                          ),
-                                        )
-                                      : const SizedBox.shrink(),
-                                  controller.notes[index].note.isNotEmpty
-                                      ? Padding(
-                                          padding: const EdgeInsets.only(
-                                            bottom: 8.0,
-                                          ),
-                                          child: Text(
-                                            controller.notes[index].note,
-                                            maxLines: 4,
-                                            overflow: TextOverflow.ellipsis,
-                                            style: const TextStyle(
-                                              fontSize: 16.0,
-                                              color: Colors.black54,
-                                            ),
-                                          ),
-                                        )
-                                      : const SizedBox.shrink(),
-                                  Padding(
-                                    padding: const EdgeInsets.only(
-                                      top: 8.0,
-                                    ),
-                                    child: Text(
-                                      controller.notes[index].dateTime
-                                          .toString(),
-                                      style: const TextStyle(
-                                        fontSize: 12.0,
-                                        color: Colors.black45,
-                                      ),
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ),
-                        ),
+                        child: NoteCard(index: index),
                       ),
                     ),
                   );
--- lib/widgets/listview_note_card.dart
+++ lib/widgets/listview_note_card.dart
@@ -2,8 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 import 'package:get/get.dart';
 import 'package:notes_getx/controllers/note_controller.dart';
-import 'package:notes_getx/screens/edit_note.dart';
-import 'package:notes_getx/widgets/delete_dialog.dart';
+import 'package:notes_getx/widgets/note_card.dart';
 
 class ListViewNoteCard extends StatelessWidget {
   ListViewNoteCard({Key? key}) : super(key: key);
@@ -22,64 +21,7 @@ class ListViewNoteCard extends StatelessWidget {
               duration: const Duration(milliseconds: 300),
               child: ScaleAnimation(
                 child: FadeInAnimation(
-                  child: Card(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(16.0),
-                    ),
-                    child: InkWell(
-                      mouseCursor: SystemMouseCursors.click,
-                      borderRadius: BorderRadius.circular(16.0),
-                      onTap: () {
-                        Get.to(() => EditNote(), arguments: index);
-                      },
-                      onLongPress: () {
-                        Get.dialog(DeleteDialog(index: index));
-                      },
-                      child: Padding(
-                        padding: const EdgeInsets.all(16.0),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            controller.notes[index].title!.isNotEmpty
-                                ? Padding(
-                                    padding: const EdgeInsets.only(bottom: 8.0),
-                                    child: Text(
-                                      controller.notes[index].title!,
-                                      maxLines: 1,
-                                      overflow: TextOverflow.ellipsis,
-                                      style: const TextStyle(
-                                        fontSize: 18.0,
-                                        fontWeight: FontWeight.bold,
-                                      ),
-                                    ),
-                                  )
-                                : const SizedBox.shrink(),
-                            controller.notes[index].note.isNotEmpty
-                                ? Padding(
-                                    padding: const EdgeInsets.only(bottom: 8.0),
-                                    child: Text(
-                                      controller.notes[index].note,
-                                      overflow: TextOverflow.ellipsis,
-                                      style: const TextStyle(
-                                        fontSize: 18.0,
-                                        color: Colors.black54,
-                                      ),
-                                    ),
-                                  )
-                                : const SizedBox.shrink(),
-                            Text(
-                              controller.notes[index].dateTime.toString(),
-                              style: const TextStyle(
-                                fontSize: 12.0,
-                                color: Colors.black45,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ),
+                  child: NoteCard(index: index),
                 ),
               ),
             ),
