629d683401e27c545e493ad86cb8823bf945173a
Ali Maddahi <alimaddahi99@gmail.com>
2022-07-10 20:50:32 +0430

    implement flutter_staggered_animations in gridview_note_card

--- lib/widgets/gridview_note_card.dart
+++ lib/widgets/gridview_note_card.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 import 'package:notes_getx/controllers/note_controller.dart';
 import 'package:notes_getx/screens/edit_note.dart';
 import 'package:notes_getx/widgets/delete_dialog.dart';
@@ -15,74 +16,92 @@ class GridViewNoteCard extends StatelessWidget {
     return LayoutBuilder(
       builder: (BuildContext context, BoxConstraints constraints) {
         return Obx(
-          () => MasonryGridView.count(
-              padding: const EdgeInsets.all(8.0),
-              crossAxisCount: constraints.maxWidth > 768 ? 4 : 2,
-              itemCount: controller.notes.length,
-              itemBuilder: (context, index) {
-                return MouseRegion(
-                  cursor: SystemMouseCursors.click,
-                  child: GestureDetector(
-                    onTap: () {
-                      Get.to(() => EditNote(), arguments: index);
-                    },
-                    onLongPress: () {
-                      Get.dialog(DeleteDialog(index: index));
-                    },
-                    child: Card(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(16.0),
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.all(16.0),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            controller.notes[index].title!.isNotEmpty
-                                ? Padding(
-                                    padding: const EdgeInsets.only(bottom: 8.0),
-                                    child: Text(
-                                      controller.notes[index].title!,
-                                      maxLines: 1,
-                                      overflow: TextOverflow.ellipsis,
-                                      style: const TextStyle(
-                                        fontSize: 18.0,
-                                        fontWeight: FontWeight.bold,
+          () => AnimationLimiter(
+            child: MasonryGridView.count(
+                padding: const EdgeInsets.all(8.0),
+                crossAxisCount: constraints.maxWidth > 768 ? 4 : 2,
+                itemCount: controller.notes.length,
+                itemBuilder: (context, index) {
+                  return AnimationConfiguration.staggeredGrid(
+                    columnCount: constraints.maxWidth > 768 ? 4 : 2,
+                    position: index,
+                    duration: const Duration(milliseconds: 300),
+                    child: ScaleAnimation(
+                      child: FadeInAnimation(
+                        child: MouseRegion(
+                          cursor: SystemMouseCursors.click,
+                          child: GestureDetector(
+                            onTap: () {
+                              Get.to(() => EditNote(), arguments: index);
+                            },
+                            onLongPress: () {
+                              Get.dialog(DeleteDialog(index: index));
+                            },
+                            child: Card(
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(16.0),
+                              ),
+                              child: Padding(
+                                padding: const EdgeInsets.all(16.0),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    controller.notes[index].title!.isNotEmpty
+                                        ? Padding(
+                                            padding: const EdgeInsets.only(
+                                              bottom: 8.0,
+                                            ),
+                                            child: Text(
+                                              controller.notes[index].title!,
+                                              maxLines: 1,
+                                              overflow: TextOverflow.ellipsis,
+                                              style: const TextStyle(
+                                                fontSize: 18.0,
+                                                fontWeight: FontWeight.bold,
+                                              ),
+                                            ),
+                                          )
+                                        : const SizedBox.shrink(),
+                                    controller.notes[index].note.isNotEmpty
+                                        ? Padding(
+                                            padding: const EdgeInsets.only(
+                                              bottom: 8.0,
+                                            ),
+                                            child: Text(
+                                              controller.notes[index].note,
+                                              maxLines: 4,
+                                              overflow: TextOverflow.ellipsis,
+                                              style: const TextStyle(
+                                                fontSize: 16.0,
+                                                color: Colors.black54,
+                                              ),
+                                            ),
+                                          )
+                                        : const SizedBox.shrink(),
+                                    Padding(
+                                      padding: const EdgeInsets.only(
+                                        top: 8.0,
+                                      ),
+                                      child: Text(
+                                        controller.notes[index].dateTime
+                                            .toString(),
+                                        style: const TextStyle(
+                                          fontSize: 12.0,
+                                          color: Colors.black45,
+                                        ),
                                       ),
                                     ),
-                                  )
-                                : const SizedBox.shrink(),
-                            controller.notes[index].note.isNotEmpty
-                                ? Padding(
-                                    padding: const EdgeInsets.only(bottom: 8.0),
-                                    child: Text(
-                                      controller.notes[index].note,
-                                      maxLines: 4,
-                                      overflow: TextOverflow.ellipsis,
-                                      style: const TextStyle(
-                                        fontSize: 16.0,
-                                        color: Colors.black54,
-                                      ),
-                                    ),
-                                  )
-                                : const SizedBox.shrink(),
-                            Padding(
-                              padding: const EdgeInsets.only(top: 8.0),
-                              child: Text(
-                                controller.notes[index].dateTime.toString(),
-                                style: const TextStyle(
-                                  fontSize: 12.0,
-                                  color: Colors.black45,
+                                  ],
                                 ),
                               ),
                             ),
-                          ],
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                );
-              }),
+                  );
+                }),
+          ),
         );
       },
     );
