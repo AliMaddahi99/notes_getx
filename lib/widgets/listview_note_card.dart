aab77e4a86e6db317bcba683664d6a01109b95f8
Ali Maddahi <alimaddahi99@gmail.com>
2022-07-10 20:56:21 +0430

    implement flutter_staggered_animations in listview_note_card

--- lib/widgets/listview_note_card.dart
+++ lib/widgets/listview_note_card.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 import 'package:get/get.dart';
 import 'package:notes_getx/controllers/note_controller.dart';
 import 'package:notes_getx/screens/edit_note.dart';
@@ -15,60 +16,72 @@ class ListViewNoteCard extends StatelessWidget {
       () => ListView.builder(
         padding: const EdgeInsets.all(8.0),
         itemBuilder: (context, index) {
-          return MouseRegion(
-            cursor: SystemMouseCursors.click,
-            child: GestureDetector(
-              onTap: () {
-                Get.to(() => EditNote(), arguments: index);
-              },
-              onLongPress: () {
-                Get.dialog(DeleteDialog(index: index));
-              },
-              child: Card(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(16.0),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      controller.notes[index].title!.isNotEmpty
-                          ? Padding(
-                              padding: const EdgeInsets.only(bottom: 8.0),
-                              child: Text(
-                                controller.notes[index].title!,
-                                maxLines: 1,
-                                overflow: TextOverflow.ellipsis,
+          return AnimationLimiter(
+            child: AnimationConfiguration.staggeredList(
+              position: index,
+              duration: const Duration(milliseconds: 300),
+              child: ScaleAnimation(
+                child: FadeInAnimation(
+                  child: MouseRegion(
+                    cursor: SystemMouseCursors.click,
+                    child: GestureDetector(
+                      onTap: () {
+                        Get.to(() => EditNote(), arguments: index);
+                      },
+                      onLongPress: () {
+                        Get.dialog(DeleteDialog(index: index));
+                      },
+                      child: Card(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(16.0),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              controller.notes[index].title!.isNotEmpty
+                                  ? Padding(
+                                      padding:
+                                          const EdgeInsets.only(bottom: 8.0),
+                                      child: Text(
+                                        controller.notes[index].title!,
+                                        maxLines: 1,
+                                        overflow: TextOverflow.ellipsis,
+                                        style: const TextStyle(
+                                          fontSize: 18.0,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                    )
+                                  : const SizedBox.shrink(),
+                              controller.notes[index].note.isNotEmpty
+                                  ? Padding(
+                                      padding:
+                                          const EdgeInsets.only(bottom: 8.0),
+                                      child: Text(
+                                        controller.notes[index].note,
+                                        overflow: TextOverflow.ellipsis,
+                                        style: const TextStyle(
+                                          fontSize: 18.0,
+                                          color: Colors.black54,
+                                        ),
+                                      ),
+                                    )
+                                  : const SizedBox.shrink(),
+                              Text(
+                                controller.notes[index].dateTime.toString(),
                                 style: const TextStyle(
-                                  fontSize: 18.0,
-                                  fontWeight: FontWeight.bold,
+                                  fontSize: 12.0,
+                                  color: Colors.black45,
                                 ),
                               ),
-                            )
-                          : const SizedBox.shrink(),
-                      controller.notes[index].note.isNotEmpty
-                          ? Padding(
-                              padding: const EdgeInsets.only(bottom: 8.0),
-                              child: Text(
-                                controller.notes[index].note,
-                                overflow: TextOverflow.ellipsis,
-                                style: const TextStyle(
-                                  fontSize: 18.0,
-                                  color: Colors.black54,
-                                ),
-                              ),
-                            )
-                          : const SizedBox.shrink(),
-                      Text(
-                        controller.notes[index].dateTime.toString(),
-                        style: const TextStyle(
-                          fontSize: 12.0,
-                          color: Colors.black45,
+                            ],
+                          ),
                         ),
                       ),
-                    ],
+                    ),
                   ),
                 ),
               ),
