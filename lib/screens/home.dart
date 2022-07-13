3e3a9f44d846d3ea13fad2be02f0471df7f24135
Ali Maddahi <alimaddahi99@gmail.com>
2022-07-13 16:39:25 +0430

    put main_appbar and select_mode_appbar in home and some changes for multiselect mode

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -4,49 +4,39 @@ import 'package:notes_getx/controllers/note_controller.dart';
 import 'package:notes_getx/screens/add_note.dart';
 import 'package:notes_getx/widgets/gridview_note_card.dart';
 import 'package:notes_getx/widgets/listview_note_card.dart';
+import 'package:notes_getx/widgets/main_appbar.dart';
+import 'package:notes_getx/widgets/select_mode_appbar.dart';
 
 class Home extends StatelessWidget {
   Home({Key? key}) : super(key: key);
 
-  final NoteController controller = Get.put(NoteController());
+  final NoteController _controller = Get.put(NoteController());
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Notes Getx"),
-        actions: [
-          Obx(
-            () => Padding(
-              padding: const EdgeInsets.only(right: 10.0),
-              child: IconButton(
-                onPressed: () {
-                  controller.isGridView.value = !controller.isGridView.value;
-                },
-                icon: controller.isGridView.value
-                    ? const Icon(Icons.view_list_rounded)
-                    : const Icon(Icons.grid_view_rounded),
-                tooltip:
-                    controller.isGridView.value ? "List view" : "Grid view",
-              ),
-            ),
-          ),
-        ],
-      ),
-      body: Obx(
-        () => controller.isGridView.value
+    return Obx(
+      () => Scaffold(
+        appBar: PreferredSize(
+          preferredSize: const Size.fromHeight(kToolbarHeight),
+          child: _controller.isSelectMode.value
+              ? SelectModeAppBar()
+              : MainAppBar(),
+        ),
+        body: _controller.isGridView.value
             ? GridViewNoteCard()
             : ListViewNoteCard(),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => {
-          Get.to(
-            () => AddNote(),
-            transition: Transition.cupertino,
-          ),
-        },
-        tooltip: "Add note",
-        child: const Icon(Icons.add),
+        floatingActionButton: !_controller.isSelectMode.value
+            ? FloatingActionButton(
+                onPressed: () => {
+                  Get.to(
+                    () => AddNote(),
+                    transition: Transition.cupertino,
+                  ),
+                },
+                tooltip: "Add note",
+                child: const Icon(Icons.add),
+              )
+            : const SizedBox.shrink(),
       ),
     );
   }
