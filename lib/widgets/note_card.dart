726831f1518dc174b63ac57bb838bc30f18cf99a
Ali Maddahi <alimaddahi99@gmail.com>
2022-07-13 16:37:27 +0430

    add checkbox and some changes to multiselect and multidelete

--- lib/widgets/note_card.dart
+++ lib/widgets/note_card.dart
@@ -8,7 +8,7 @@ class NoteCard extends StatelessWidget {
   final int index;
   NoteCard({Key? key, required this.index}) : super(key: key);
 
-  final NoteController controller = Get.find();
+  final NoteController _controller = Get.find();
 
   @override
   Widget build(BuildContext context) {
@@ -16,65 +16,93 @@ class NoteCard extends StatelessWidget {
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(16.0),
       ),
-      child: InkWell(
-        mouseCursor: SystemMouseCursors.click,
-        borderRadius: BorderRadius.circular(16.0),
-        onTap: () {
-          Get.to(() => EditNote(), arguments: index);
-        },
-        onLongPress: () {
-          Get.dialog(DeleteDialog(index: index));
-        },
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              controller.notes[index].title!.isNotEmpty
-                  ? Padding(
-                      padding: const EdgeInsets.only(
-                        bottom: 8.0,
+      child: Obx(
+        () => InkWell(
+          mouseCursor: SystemMouseCursors.click,
+          borderRadius: BorderRadius.circular(16.0),
+          onTap: () {
+            _controller.isSelectMode.value
+                ? _controller.selectNote(_controller.notes[index].id)
+                : Get.to(() => EditNote(), arguments: index);
+          },
+          onLongPress: () {
+            // Get.dialog(DeleteDialog(index: index));
+            _controller.isSelectMode.value = true;
+            _controller.selectNote(_controller.notes[index].id);
+          },
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Stack(
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    _controller.notes[index].title!.isNotEmpty
+                        ? Padding(
+                            padding: const EdgeInsets.only(
+                              bottom: 8.0,
+                            ),
+                            child: Text(
+                              _controller.notes[index].title!,
+                              maxLines: 1,
+                              overflow: TextOverflow.ellipsis,
+                              style: const TextStyle(
+                                fontSize: 18.0,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          )
+                        : const SizedBox.shrink(),
+                    _controller.notes[index].note.isNotEmpty
+                        ? Padding(
+                            padding: const EdgeInsets.only(
+                              bottom: 8.0,
+                            ),
+                            child: Text(
+                              _controller.notes[index].note,
+                              maxLines: _controller.isGridView.value ? 4 : 1,
+                              overflow: TextOverflow.ellipsis,
+                              style: TextStyle(
+                                fontSize:
+                                    _controller.isGridView.value ? 16.0 : 18.0,
+                                color: Colors.black54,
+                              ),
+                            ),
+                          )
+                        : const SizedBox.shrink(),
+                    Padding(
+                      padding: EdgeInsets.only(
+                        top: _controller.isGridView.value ? 8.0 : 0,
                       ),
                       child: Text(
-                        controller.notes[index].title!,
-                        maxLines: 1,
-                        overflow: TextOverflow.ellipsis,
+                        _controller.notes[index].dateTime.toString(),
                         style: const TextStyle(
-                          fontSize: 18.0,
-                          fontWeight: FontWeight.bold,
+                          fontSize: 12.0,
+                          color: Colors.black45,
                         ),
                       ),
-                    )
-                  : const SizedBox.shrink(),
-              controller.notes[index].note.isNotEmpty
-                  ? Padding(
-                      padding: const EdgeInsets.only(
-                        bottom: 8.0,
-                      ),
-                      child: Text(
-                        controller.notes[index].note,
-                        maxLines: controller.isGridView.value ? 4 : 1,
-                        overflow: TextOverflow.ellipsis,
-                        style: TextStyle(
-                          fontSize: controller.isGridView.value ? 16.0 : 18.0,
-                          color: Colors.black54,
-                        ),
-                      ),
-                    )
-                  : const SizedBox.shrink(),
-              Padding(
-                padding: EdgeInsets.only(
-                  top: controller.isGridView.value ? 8.0 : 0,
+                    ),
+                  ],
                 ),
-                child: Text(
-                  controller.notes[index].dateTime.toString(),
-                  style: const TextStyle(
-                    fontSize: 12.0,
-                    color: Colors.black45,
+                Visibility(
+                  visible: _controller.isSelectMode.value,
+                  child: Positioned(
+                    right: -6,
+                    bottom: -6,
+                    child: Checkbox(
+                        splashRadius: 0,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20.0),
+                        ),
+                        value: _controller.selectedNote
+                            .contains(_controller.notes[index].id),
+                        onChanged: (checked) {
+                          _controller.selectNote(_controller.notes[index].id);
+                        }),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
