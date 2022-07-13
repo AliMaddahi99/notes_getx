ac8a52198099ca106f830bb475336b301a0c83e2
Ali Maddahi <alimaddahi99@gmail.com>
2022-07-13 16:38:22 +0430

    create select_mode_appbar

---
+++ lib/widgets/select_mode_appbar.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:notes_getx/controllers/note_controller.dart';
+
+class SelectModeAppBar extends StatelessWidget {
+  SelectModeAppBar({Key? key}) : super(key: key);
+
+  final NoteController _controller = Get.find();
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      title: Obx(
+        () => Text(
+          _controller.selectedNote.isEmpty
+              ? "Select items"
+              : _controller.getSelectedNotesCount(),
+        ),
+      ),
+      centerTitle: true,
+      leading: IconButton(
+        icon: const Icon(Icons.delete_rounded),
+        onPressed: () {
+          for (var n in _controller.selectedNote) {
+            _controller.deleteNote(n);
+          }
+          _controller.selectedNote.clear();
+          _controller.isSelectMode.value = false;
+        },
+      ),
+      actions: [
+        IconButton(
+          onPressed: () {
+            if (_controller.selectedNote.length < _controller.notes.length) {
+              _controller.selectedNote.clear();
+              for (var note in _controller.notes) {
+                _controller.selectedNote.add(note.id);
+              }
+            } else {
+              _controller.selectedNote.clear();
+            }
+          },
+          icon: const Icon(Icons.checklist_rounded),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(right: 10.0),
+          child: IconButton(
+            icon: const Icon(Icons.clear_rounded),
+            onPressed: () {
+              _controller.isSelectMode.value = false;
+              _controller.selectedNote.clear();
+            },
+          ),
+        ),
+      ],
+    );
+  }
+}
