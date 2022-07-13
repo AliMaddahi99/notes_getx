221360dd99773e7fb72368bc3d3077e4f4159c0c
Ali Maddahi <alimaddahi99@gmail.com>
2022-07-13 19:49:10 +0430

    remove useless stack, change padding top of dateTime and change checkbox layout position in note_card

--- lib/widgets/note_card.dart
+++ lib/widgets/note_card.dart
@@ -30,73 +30,78 @@ class NoteCard extends StatelessWidget {
           },
           child: Padding(
             padding: const EdgeInsets.all(16.0),
-            child: Stack(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    _controller.notes[index].title!.isNotEmpty
-                        ? Padding(
-                            padding: const EdgeInsets.only(
-                              bottom: 8.0,
-                            ),
-                            child: Text(
-                              _controller.notes[index].title!,
-                              maxLines: 1,
-                              overflow: TextOverflow.ellipsis,
-                              style: const TextStyle(
-                                fontSize: 18.0,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                          )
-                        : const SizedBox.shrink(),
-                    _controller.notes[index].note.isNotEmpty
-                        ? Padding(
-                            padding: const EdgeInsets.only(
-                              bottom: 8.0,
-                            ),
-                            child: Text(
-                              _controller.notes[index].note,
-                              maxLines: _controller.isGridView.value ? 4 : 1,
-                              overflow: TextOverflow.ellipsis,
-                              style: TextStyle(
-                                fontSize:
-                                    _controller.isGridView.value ? 16.0 : 18.0,
-                                color: Colors.black54,
-                              ),
-                            ),
-                          )
-                        : const SizedBox.shrink(),
-                    Padding(
-                      padding: EdgeInsets.only(
-                        top: _controller.isGridView.value ? 8.0 : 0,
-                      ),
-                      child: Text(
+                _controller.notes[index].title!.isNotEmpty
+                    ? Padding(
+                        padding: const EdgeInsets.only(
+                          bottom: 8.0,
+                        ),
+                        child: Text(
+                          _controller.notes[index].title!,
+                          maxLines: 1,
+                          overflow: TextOverflow.ellipsis,
+                          style: const TextStyle(
+                            fontSize: 18.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      )
+                    : const SizedBox.shrink(),
+                _controller.notes[index].note.isNotEmpty
+                    ? Padding(
+                        padding: const EdgeInsets.only(
+                          bottom: 8.0,
+                        ),
+                        child: Text(
+                          _controller.notes[index].note,
+                          maxLines: _controller.isGridView.value ? 4 : 1,
+                          overflow: TextOverflow.ellipsis,
+                          style: TextStyle(
+                            fontSize:
+                                _controller.isGridView.value ? 16.0 : 18.0,
+                            color: Colors.black54,
+                          ),
+                        ),
+                      )
+                    : const SizedBox.shrink(),
+                Padding(
+                  padding: EdgeInsets.only(
+                    top: _controller.isGridView.value ? 16.0 : 0,
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
                         _controller.notes[index].dateTime.toString(),
                         style: const TextStyle(
                           fontSize: 12.0,
                           color: Colors.black45,
                         ),
                       ),
-                    ),
-                  ],
-                ),
-                Visibility(
-                  visible: _controller.isSelectMode.value,
-                  child: Positioned(
-                    right: -6,
-                    bottom: -6,
-                    child: Checkbox(
-                        splashRadius: 0,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20.0),
+                      Visibility(
+                        visible: _controller.isSelectMode.value,
+                        child: Transform.scale(
+                          scale: 1.3,
+                          child: SizedBox(
+                            height: 0,
+                            width: 16,
+                            child: Checkbox(
+                                splashRadius: 0,
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(20.0),
+                                ),
+                                value: _controller.selectedNote
+                                    .contains(_controller.notes[index].id),
+                                onChanged: (checked) {
+                                  _controller
+                                      .selectNote(_controller.notes[index].id);
+                                }),
+                          ),
                         ),
-                        value: _controller.selectedNote
-                            .contains(_controller.notes[index].id),
-                        onChanged: (checked) {
-                          _controller.selectNote(_controller.notes[index].id);
-                        }),
+                      ),
+                    ],
                   ),
                 ),
               ],
