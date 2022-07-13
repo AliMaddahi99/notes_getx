4ef3e9cfa71b9e46a53ccbe89cdc86d97dd8101d
Ali Maddahi <alimaddahi99@gmail.com>
2022-07-13 16:38:03 +0430

    create main_appbar

---
+++ lib/widgets/main_appbar.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:notes_getx/controllers/note_controller.dart';
+
+class MainAppBar extends StatelessWidget {
+  MainAppBar({Key? key}) : super(key: key);
+
+  final NoteController _controller = Get.find();
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      title: const Text("Notes Getx"),
+      actions: [
+        Obx(
+          () => Padding(
+            padding: const EdgeInsets.only(right: 10.0),
+            child: IconButton(
+              onPressed: () {
+                _controller.isGridView.value = !_controller.isGridView.value;
+              },
+              icon: _controller.isGridView.value
+                  ? const Icon(Icons.view_list_rounded)
+                  : const Icon(Icons.grid_view_rounded),
+              tooltip: _controller.isGridView.value ? "List view" : "Grid view",
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
