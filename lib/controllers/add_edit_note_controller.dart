11fdad5a907b51051bc0cc8f758e6bf9840deb8c
Ali Maddahi <alimaddahi99@gmail.com>
2022-07-14 14:41:22 +0430

    move TextEditingControllers, FocusNode, addNote() and dateTimeNowStringify() to edit_note_controller. change edit_note_controller to add_edit_note_controller and some additions to work for adding and editing with same controller

---
+++ lib/controllers/add_edit_note_controller.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:notes_getx/controllers/note_controller.dart';
+import 'package:notes_getx/models/note_model.dart';
+
+class AddEditNoteController extends GetxController {
+  final TextEditingController titleTextController = TextEditingController();
+  final TextEditingController noteTextController = TextEditingController();
+  final FocusNode noteFocusNode = FocusNode();
+
+  var args = Get.arguments;
+  var isSaveButtonVisible = false.obs;
+
+  final NoteController _controller = Get.find();
+
+  void addNote({String? title, required String note}) {
+    if ((title != null || title != "") && note.isNotEmpty) {
+      _controller.notes.insert(
+        0,
+        NoteModel(
+          id: DateTime.now().microsecondsSinceEpoch,
+          note: note,
+          title: title,
+          dateTime: dateTimeNowStringify(),
+        ),
+      );
+      args = 0;
+    }
+  }
+
+  void editNote() {
+    if (titleTextController.text.isEmpty && noteTextController.text.isEmpty) {
+      _controller.deleteNote(_controller.notes[args].id);
+    } else {
+      var edited = _controller.notes[args];
+      edited.title = titleTextController.text;
+      edited.note = noteTextController.text;
+      edited.dateTime = dateTimeNowStringify();
+      _controller.notes[args] = edited;
+    }
+  }
+
+  String dateTimeNowStringify() {
+    return DateTime.now().toString().substring(0, 16).replaceAll("-", "/");
+  }
+
+  bool saveButtonVisibility() {
+    return titleTextController.text.isNotEmpty ||
+        noteTextController.text.isNotEmpty;
+  }
+
+  @override
+  void onInit() {
+    if (args != null) {
+      titleTextController.text = _controller.notes[args].title!;
+      noteTextController.text = _controller.notes[args].note;
+      isSaveButtonVisible.value = true;
+    } else {
+      noteFocusNode.requestFocus();
+    }
+    super.onInit();
+  }
+}
--- lib/controllers/note_controller.dart
+++ lib/controllers/note_controller.dart
@@ -1,49 +1,15 @@
-import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:notes_getx/models/note_model.dart';
 
 class NoteController extends GetxController {
-  final TextEditingController titleTextController = TextEditingController();
-  final TextEditingController noteTextController = TextEditingController();
-  final FocusNode noteFocusNode = FocusNode();
-
   var notes = <NoteModel>[].obs;
   var isGridView = false.obs;
   var selectedNote = [].obs;
   var isSelectMode = false.obs;
 
-  void addNote({String? title, required String note}) {
-    titleTextController.clear();
-    noteTextController.clear();
-
-    if ((title == null || title == "") && note.isEmpty) {
-      Get.snackbar(
-        "Empty title and note!",
-        "Please at least write a title or note",
-        snackPosition: SnackPosition.BOTTOM,
-      );
-    } else {
-      notes.insert(
-        0,
-        NoteModel(
-          id: DateTime.now().microsecondsSinceEpoch,
-          note: note,
-          title: title,
-          dateTime: dateTimeNowStringify(),
-        ),
-      );
-    }
-
-    noteFocusNode.requestFocus();
-  }
-
   void deleteNote(int id) {
     notes.removeWhere((note) => note.id == id);
-  }
-
-  String dateTimeNowStringify() {
-    return DateTime.now().toString().substring(0, 16).replaceAll("-", "/");
   }
 
   void selectNote(int id) {
