511fbc1186614eb06dbb18b2e111d887b6cd5e24
Ali Maddahi <alimaddahi99@gmail.com>
2022-07-16 17:51:36 +0430

    remove delete botton and rearrange icons in select_mode_app_bar

--- lib/widgets/select_mode_app_bar.dart
+++ lib/widgets/select_mode_app_bar.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:notes_getx/controllers/note_controller.dart';
-import 'package:notes_getx/widgets/delete_bottom_sheet.dart';
 
 class SelectModeAppBar extends StatelessWidget {
   SelectModeAppBar({Key? key}) : super(key: key);
@@ -19,48 +18,28 @@ class SelectModeAppBar extends StatelessWidget {
         ),
       ),
       centerTitle: true,
-      leading: Obx(
-        () => IconButton(
-          icon: const Icon(Icons.delete_rounded),
-          onPressed: _controller.selectedNote.isNotEmpty
-              ? () {
-                  Get.bottomSheet(
-                    DeleteBottomSheet(
-                      message: "Delete ${_controller.getSelectedNotesCount()}?",
-                    ),
-                    backgroundColor: Colors.white,
-                    shape: const RoundedRectangleBorder(
-                      borderRadius: BorderRadius.vertical(
-                        top: Radius.circular(20.0),
-                      ),
-                    ),
-                  );
-                }
-              : null,
-        ),
+      leading: IconButton(
+        icon: const Icon(Icons.clear_rounded),
+        onPressed: () {
+          _controller.isSelectMode.value = false;
+          _controller.selectedNote.clear();
+        },
       ),
       actions: [
-        IconButton(
-          onPressed: () {
-            if (_controller.selectedNote.length < _controller.notes.length) {
-              _controller.selectedNote.clear();
-              for (var note in _controller.notes) {
-                _controller.selectedNote.add(note.id);
-              }
-            } else {
-              _controller.selectedNote.clear();
-            }
-          },
-          icon: const Icon(Icons.checklist_rounded),
-        ),
         Padding(
           padding: const EdgeInsets.only(right: 10.0),
           child: IconButton(
-            icon: const Icon(Icons.clear_rounded),
             onPressed: () {
-              _controller.isSelectMode.value = false;
-              _controller.selectedNote.clear();
+              if (_controller.selectedNote.length < _controller.notes.length) {
+                _controller.selectedNote.clear();
+                for (var note in _controller.notes) {
+                  _controller.selectedNote.add(note.id);
+                }
+              } else {
+                _controller.selectedNote.clear();
+              }
             },
+            icon: const Icon(Icons.checklist_rounded),
           ),
         ),
       ],
