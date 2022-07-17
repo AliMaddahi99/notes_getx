215b708d6c3a062cd1ec593be926ceb77ba8b9a2
Ali Maddahi <alimaddahi99@gmail.com>
2022-07-17 20:41:19 +0430

    change main_app_bar ui and functionality

--- lib/notes/widgets/main_appbar.dart
+++ lib/notes/widgets/main_appbar.dart
@@ -10,23 +10,64 @@ class MainAppBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return AppBar(
-      title: const Text("Notes Getx"),
-      actions: [
-        Obx(
-          () => Padding(
-            padding: const EdgeInsets.only(right: 10.0),
-            child: IconButton(
-              onPressed: () {
-                _controller.isGridView.value = !_controller.isGridView.value;
-              },
-              icon: _controller.isGridView.value
-                  ? const Icon(Icons.view_list_rounded)
-                  : const Icon(Icons.grid_view_rounded),
-              tooltip: _controller.isGridView.value ? "List view" : "Grid view",
+      title: Stack(
+        alignment: Alignment.centerRight,
+        children: [
+          Obx(
+            () => Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                IconButton(
+                  onPressed: () {
+                    _controller.pageViewId.value = 0;
+                    _controller.pageController.animateToPage(
+                      0,
+                      duration: const Duration(milliseconds: 500),
+                      curve: Curves.decelerate,
+                    );
+                  },
+                  icon: Icon(
+                    _controller.pageViewId.value == 0
+                        ? Icons.book_rounded
+                        : Icons.book_outlined,
+                  ),
+                ),
+                IconButton(
+                  onPressed: () {
+                    _controller.pageViewId.value = 1;
+                    _controller.pageController.animateToPage(
+                      1,
+                      duration: const Duration(milliseconds: 500),
+                      curve: Curves.decelerate,
+                    );
+                  },
+                  icon: Icon(
+                    _controller.pageViewId.value == 1
+                        ? Icons.check_box_rounded
+                        : Icons.check_box_outlined,
+                  ),
+                ),
+              ],
             ),
           ),
-        ),
-      ],
+          Obx(
+            () => _controller.pageViewId.value == 0
+                ? IconButton(
+                    onPressed: () {
+                      _controller.isGridView.value =
+                          !_controller.isGridView.value;
+                    },
+                    icon: _controller.isGridView.value
+                        ? const Icon(Icons.view_list_rounded)
+                        : const Icon(Icons.grid_view_rounded),
+                    tooltip: _controller.isGridView.value
+                        ? "List view"
+                        : "Grid view",
+                  )
+                : const SizedBox.shrink(),
+          ),
+        ],
+      ),
     );
   }
 }
