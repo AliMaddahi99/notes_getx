fe2b30e0158b1af5404e1ff4698d712127b88787
Ali Maddahi <alimaddahi99@gmail.com>
2022-07-19 19:06:38 +0430

    remove fields and functions that was moved to app_controller

--- lib/controllers/note/note_controller.dart
+++ lib/controllers/note/note_controller.dart
@@ -1,29 +1,13 @@
-import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:notes_getx/models/note_model.dart';
 
 class NoteController extends GetxController {
-  final PageController pageController = PageController();
-
   var notes = <NoteModel>[].obs;
   var isGridView = false.obs;
-  var selectedNote = [].obs;
-  var isSelectMode = false.obs;
-  var pageViewId = 0.obs;
 
   void deleteNote(int id) {
     notes.removeWhere((note) => note.id == id);
-  }
-
-  void selectNote(int id) {
-    selectedNote.contains(id) ? selectedNote.remove(id) : selectedNote.add(id);
-  }
-
-  String getSelectedNotesCount() {
-    return selectedNote.length == 1
-        ? "${selectedNote.length} item"
-        : "${selectedNote.length} items";
   }
 
   @override
