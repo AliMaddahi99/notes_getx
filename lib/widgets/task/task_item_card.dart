42577f9333cac7bc31e24e161a8fa74553925dfe
Ali Maddahi <alimaddahi99@gmail.com>
2022-07-22 15:14:21 +0430

    replace GestureDetector with InkWell. add check box on select mode in task_item_card

--- lib/widgets/task/task_item_card.dart
+++ lib/widgets/task/task_item_card.dart
@@ -1,72 +1,95 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:notes_getx/controllers/app_controller.dart';
 import 'package:notes_getx/controllers/task/task_controller.dart';
 import 'package:notes_getx/screens/task/add_edit_task.dart';
 
 class TaskItemCard extends StatelessWidget {
   final int index;
+  TaskItemCard({Key? key, required this.index}) : super(key: key);
 
-  TaskItemCard({
-    Key? key,
-    required this.index,
-  }) : super(key: key);
-
+  final AppController _appController = Get.find();
   final TaskController _taskController = Get.find();
 
   @override
   Widget build(BuildContext context) {
-    return MouseRegion(
-      cursor: SystemMouseCursors.click,
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(16.0),
+      ),
       child: Obx(
-        () => GestureDetector(
+        () => InkWell(
+          mouseCursor: SystemMouseCursors.click,
+          borderRadius: BorderRadius.circular(16.0),
           onTap: () {
-            Get.to(
-              () => AddEditTask(),
-              opaque: false,
-              arguments: index,
-            );
-            _taskController.isAddEditTaskScreen.value = true;
+            if (_appController.isSelectMode.value) {
+              _appController.selectItem(_taskController.tasks[index].id);
+            } else {
+              _taskController.isAddEditTaskScreen.value = true;
+              _appController.selectedItems.clear();
+              Get.to(
+                () => AddEditTask(),
+                opaque: false,
+                arguments: index,
+              );
+            }
           },
-          child: Card(
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(16.0),
-            ),
-            margin: const EdgeInsets.all(0),
-            child: Container(
-              padding: const EdgeInsets.fromLTRB(5.0, 10.0, 10.0, 10.0),
-              child: Row(
-                children: [
-                  Obx(
-                    () => Checkbox(
-                      activeColor: Colors.grey,
+          onLongPress: () {
+            _appController.isSelectMode.value = true;
+            _appController.selectItem(_taskController.tasks[index].id);
+          },
+          child: Container(
+            padding: const EdgeInsets.fromLTRB(5.0, 10.0, 10.0, 10.0),
+            child: Row(
+              children: [
+                Obx(
+                  () => Checkbox(
+                    activeColor: Colors.grey,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(3.0),
+                    ),
+                    value: _taskController.tasks[index].isDone,
+                    onChanged: (done) {
+                      var changed = _taskController.tasks[index];
+                      changed.isDone = done!;
+                      _taskController.tasks[index] = changed;
+                    },
+                  ),
+                ),
+                Expanded(
+                  child: Text(
+                    _taskController.tasks[index].title,
+                    style: _taskController.tasks[index].isDone
+                        ? const TextStyle(
+                            decoration: TextDecoration.lineThrough,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.grey,
+                          )
+                        : const TextStyle(
+                            decoration: TextDecoration.none,
+                            fontWeight: FontWeight.bold,
+                          ),
+                  ),
+                ),
+                Visibility(
+                  visible: _appController.isSelectMode.value,
+                  child: Transform.scale(
+                    scale: 1.3,
+                    child: Checkbox(
+                      splashRadius: 0,
                       shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(3.0),
+                        borderRadius: BorderRadius.circular(20.0),
                       ),
-                      value: _taskController.tasks[index].isDone,
-                      onChanged: (done) {
-                        var changed = _taskController.tasks[index];
-                        changed.isDone = done!;
-                        _taskController.tasks[index] = changed;
+                      value: _appController.selectedItems
+                          .contains(_taskController.tasks[index].id),
+                      onChanged: (checked) {
+                        _appController
+                            .selectItem(_taskController.tasks[index].id);
                       },
                     ),
                   ),
-                  Expanded(
-                    child: Text(
-                      _taskController.tasks[index].title,
-                      style: _taskController.tasks[index].isDone
-                          ? const TextStyle(
-                              decoration: TextDecoration.lineThrough,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.grey,
-                            )
-                          : const TextStyle(
-                              decoration: TextDecoration.none,
-                              fontWeight: FontWeight.bold,
-                            ),
-                    ),
-                  ),
-                ],
-              ),
+                ),
+              ],
             ),
           ),
         ),
