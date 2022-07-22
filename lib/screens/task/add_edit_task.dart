21f517ed929e393042046963f93041cbb31053d8
Ali Maddahi <alimaddahi99@gmail.com>
2022-07-22 18:34:35 +0430

    remove separate add_edit_task_text_field and implement it directly in add_edit_task screen

--- lib/screens/task/add_edit_task.dart
+++ lib/screens/task/add_edit_task.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:notes_getx/controllers/task/add_edit_task_controller.dart';
-import 'package:notes_getx/widgets/task/add_edit_task_text_field.dart';
 
 class AddEditTask extends StatelessWidget {
   AddEditTask({Key? key}) : super(key: key);
@@ -25,8 +24,75 @@ class AddEditTask extends StatelessWidget {
           body: Column(
             mainAxisAlignment: MainAxisAlignment.end,
             children: [
-              SizedBox(
-                child: AddEditTaskTextField(),
+              Container(
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.vertical(
+                    top: Radius.circular(16.0),
+                  ),
+                ),
+                child: Obx(
+                  () => Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Container(
+                        margin: const EdgeInsets.symmetric(horizontal: 10.0),
+                        child: TextField(
+                          minLines: 1,
+                          maxLines: 12,
+                          maxLength: 2000,
+                          decoration: const InputDecoration(
+                            border: InputBorder.none,
+                            hintText: "What todo?",
+                          ),
+                          controller: _addEditTaskController.taskTextController,
+                          focusNode: _addEditTaskController.taskFocusNode,
+                          toolbarOptions: const ToolbarOptions(
+                            copy: true,
+                            cut: true,
+                            paste: true,
+                            selectAll: true,
+                          ),
+                          onChanged: (text) {
+                            _addEditTaskController.isTextFieldNotEmpty.value =
+                                text.isNotEmpty;
+                          },
+                          onSubmitted:
+                              _addEditTaskController.isTextFieldNotEmpty.value
+                                  ? (text) {
+                                      _addEditTaskController.addOrEdit();
+                                    }
+                                  : null,
+                        ),
+                      ),
+                      Container(
+                        color: Colors.white,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            TextButton(
+                              onPressed: _addEditTaskController
+                                      .isTextFieldNotEmpty.value
+                                  ? () {
+                                      _addEditTaskController.addOrEdit();
+                                    }
+                                  : () {},
+                              child: Text(
+                                "Done",
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    color: _addEditTaskController
+                                            .isTextFieldNotEmpty.value
+                                        ? Colors.amber
+                                        : Colors.grey),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
               ),
             ],
           ),
