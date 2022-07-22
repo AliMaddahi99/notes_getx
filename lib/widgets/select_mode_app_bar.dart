21393f5be6ae059497bb274cbcdb4e4e3b8a9b93
Ali Maddahi <alimaddahi99@gmail.com>
2022-07-22 15:10:41 +0430

    implement select and deselect all tasks in select_mode_app_bar

--- lib/widgets/select_mode_app_bar.dart
+++ lib/widgets/select_mode_app_bar.dart
@@ -2,12 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:notes_getx/controllers/app_controller.dart';
 import 'package:notes_getx/controllers/note/note_controller.dart';
+import 'package:notes_getx/controllers/task/task_controller.dart';
 
 class SelectModeAppBar extends StatelessWidget {
   SelectModeAppBar({Key? key}) : super(key: key);
 
-  final NoteController _noteController = Get.find();
   final AppController _appController = Get.find();
+  final NoteController _noteController = Get.find();
+  final TaskController _taskController = Get.find();
 
   @override
   Widget build(BuildContext context) {
@@ -32,11 +34,19 @@ class SelectModeAppBar extends StatelessWidget {
           padding: const EdgeInsets.only(right: 10.0),
           child: IconButton(
             onPressed: () {
-              if (_appController.selectedItems.length <
-                  _noteController.notes.length) {
+              if (_appController.pageViewId.value == 0 &&
+                  _appController.selectedItems.length <
+                      _noteController.notes.length) {
                 _appController.selectedItems.clear();
                 for (var note in _noteController.notes) {
                   _appController.selectedItems.add(note.id);
+                }
+              } else if (_appController.pageViewId.value == 1 &&
+                  _appController.selectedItems.length <
+                      _taskController.tasks.length) {
+                _appController.selectedItems.clear();
+                for (var task in _taskController.tasks) {
+                  _appController.selectedItems.add(task.id);
                 }
               } else {
                 _appController.selectedItems.clear();
