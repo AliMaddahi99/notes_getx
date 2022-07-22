d4447bd0a5f68216ec5972fd5dd1710451f2ae93
Ali Maddahi <alimaddahi99@gmail.com>
2022-07-22 17:10:51 +0430

    set task_home fab visibility

--- lib/screens/task/task_home.dart
+++ lib/screens/task/task_home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:notes_getx/controllers/app_controller.dart';
 import 'package:notes_getx/controllers/task/task_controller.dart';
 import 'package:notes_getx/screens/task/add_edit_task.dart';
 import 'package:notes_getx/widgets/task/dismissible_card_listview.dart';
@@ -7,6 +8,7 @@ import 'package:notes_getx/widgets/task/dismissible_card_listview.dart';
 class TaskHome extends StatelessWidget {
   TaskHome({Key? key}) : super(key: key);
 
+  final AppController _appController = Get.find();
   final TaskController _taskController = Get.find();
 
   @override
@@ -16,16 +18,19 @@ class TaskHome extends StatelessWidget {
         body: SizedBox(
           child: DismissibleCardListView(),
         ),
-        floatingActionButton: !_taskController.isAddEditTaskScreen.value
-            ? FloatingActionButton(
-                onPressed: () {
-                  Get.to(
-                    () => AddEditTask(),
-                    opaque: false,
-                  );
-                  _taskController.isAddEditTaskScreen.value = true;
-                },
-                child: const Icon(Icons.add_rounded),
+        floatingActionButton: !_appController.isSelectMode.value
+            ? Visibility(
+                visible: !_taskController.isAddEditTaskScreen.value,
+                child: FloatingActionButton(
+                  onPressed: () {
+                    Get.to(
+                      () => AddEditTask(),
+                      opaque: false,
+                    );
+                    _taskController.isAddEditTaskScreen.value = true;
+                  },
+                  child: const Icon(Icons.add_rounded),
+                ),
               )
             : const SizedBox.shrink(),
       ),
