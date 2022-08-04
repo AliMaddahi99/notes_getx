6739ebea86d46f9e4a196a754f233ade64d8dc14
Ali Maddahi <alimaddahi99@gmail.com>
2022-08-04 16:20:36 +0430

    replace SizedBox(width:double.infinity) with Row in note_card

--- lib/widgets/note/note_card.dart
+++ lib/widgets/note/note_card.dart
@@ -18,87 +18,85 @@ class NoteCard extends StatelessWidget {
           ? Alignment.bottomRight
           : Alignment.centerRight,
       children: [
-        // SizedBox with width: double.infinity to override stack's width
-        SizedBox(
-          width: double.infinity,
-          child: Card(
-            shape: RoundedRectangleBorder(
+        Card(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(16.0),
+          ),
+          child: Obx(
+            () => InkWell(
+              mouseCursor: SystemMouseCursors.click,
               borderRadius: BorderRadius.circular(16.0),
-            ),
-            child: Obx(
-              () => InkWell(
-                mouseCursor: SystemMouseCursors.click,
-                borderRadius: BorderRadius.circular(16.0),
-                onTap: () {
-                  if (_appController.isSelectMode.value) {
-                    _appController.selectItem(_noteController.notes[index].id);
-                  } else {
-                    _appController.selectedItems.clear();
-                    _appController.selectItem(_noteController.notes[index].id);
-                    Get.to(
-                      () => AddEditNote(),
-                      arguments: index,
-                    );
-                  }
-                },
-                onLongPress: () {
-                  _appController.isSelectMode.value = true;
+              onTap: () {
+                if (_appController.isSelectMode.value) {
                   _appController.selectItem(_noteController.notes[index].id);
-                },
-                child: Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      _noteController.notes[index].title.isNotEmpty
-                          ? Padding(
-                              padding: const EdgeInsets.only(
-                                bottom: 8.0,
+                } else {
+                  _appController.selectedItems.clear();
+                  _appController.selectItem(_noteController.notes[index].id);
+                  Get.to(
+                    () => AddEditNote(),
+                    arguments: index,
+                  );
+                }
+              },
+              onLongPress: () {
+                _appController.isSelectMode.value = true;
+                _appController.selectItem(_noteController.notes[index].id);
+              },
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    _noteController.notes[index].title.isNotEmpty
+                        ? Padding(
+                            padding: const EdgeInsets.only(
+                              bottom: 8.0,
+                            ),
+                            child: Text(
+                              _noteController.notes[index].title,
+                              maxLines: 1,
+                              overflow: TextOverflow.ellipsis,
+                              style: const TextStyle(
+                                fontSize: 18.0,
+                                fontWeight: FontWeight.bold,
                               ),
-                              child: Text(
-                                _noteController.notes[index].title,
-                                maxLines: 1,
-                                overflow: TextOverflow.ellipsis,
-                                style: const TextStyle(
-                                  fontSize: 18.0,
-                                  fontWeight: FontWeight.bold,
-                                ),
+                            ),
+                          )
+                        : const SizedBox.shrink(),
+                    _noteController.notes[index].note.isNotEmpty
+                        ? Padding(
+                            padding: const EdgeInsets.only(
+                              bottom: 8.0,
+                            ),
+                            child: Text(
+                              _noteController.notes[index].note,
+                              maxLines:
+                                  _noteController.isGridView.value ? 4 : 1,
+                              overflow: TextOverflow.ellipsis,
+                              style: TextStyle(
+                                fontSize: _noteController.isGridView.value
+                                    ? 16.0
+                                    : 18.0,
+                                color: Colors.black54,
                               ),
-                            )
-                          : const SizedBox.shrink(),
-                      _noteController.notes[index].note.isNotEmpty
-                          ? Padding(
-                              padding: const EdgeInsets.only(
-                                bottom: 8.0,
-                              ),
-                              child: Text(
-                                _noteController.notes[index].note,
-                                maxLines:
-                                    _noteController.isGridView.value ? 4 : 1,
-                                overflow: TextOverflow.ellipsis,
-                                style: TextStyle(
-                                  fontSize: _noteController.isGridView.value
-                                      ? 16.0
-                                      : 18.0,
-                                  color: Colors.black54,
-                                ),
-                              ),
-                            )
-                          : const SizedBox.shrink(),
-                      Padding(
-                        padding: EdgeInsets.only(
-                          top: _noteController.isGridView.value ? 16.0 : 0,
-                        ),
-                        child: Text(
-                          _noteController.notes[index].dateTime.toString(),
-                          style: const TextStyle(
-                            fontSize: 12.0,
-                            color: Colors.black45,
-                          ),
+                            ),
+                          )
+                        : const SizedBox.shrink(),
+                    Padding(
+                      padding: EdgeInsets.only(
+                        top: _noteController.isGridView.value ? 16.0 : 0,
+                      ),
+                      child: Text(
+                        _noteController.notes[index].dateTime.toString(),
+                        style: const TextStyle(
+                          fontSize: 12.0,
+                          color: Colors.black45,
                         ),
                       ),
-                    ],
-                  ),
+                    ),
+                    // Empty Row to override stack's width
+                    Row(),
+                  ],
                 ),
               ),
             ),
