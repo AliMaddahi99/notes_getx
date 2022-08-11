27b564676345665ba2c82601b3942711828ef49c
Ali Maddahi <alimaddahi99@gmail.com>
2022-08-11 23:20:41 +0430

    add TextEditingController to folder_controller

--- lib/controllers/note/folder_controller.dart
+++ lib/controllers/note/folder_controller.dart
@@ -1,8 +1,11 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:notes_getx/models/folder_model.dart';
 import 'package:notes_getx/models/note_model.dart';
 
 class FolderController extends GetxController {
+  final TextEditingController folderTextController = TextEditingController();
+
   var folders = <FolderModel>[].obs;
 
   void createFolder(String folderName, List<NoteModel> notes) {
