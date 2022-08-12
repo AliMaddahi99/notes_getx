937a0479dd4eba78b917ac6bedd91dd722c8e2cb
Ali Maddahi <alimaddahi99@gmail.com>
2022-08-12 23:05:04 +0430

    implement FolderModel.fromJson and toJson() in folder_model to save folders in GetStorage

--- lib/models/folder_model.dart
+++ lib/models/folder_model.dart
@@ -1,8 +1,20 @@
-import 'package:notes_getx/models/note_model.dart';
-
 class FolderModel {
   final String name;
-  final List<NoteModel> notes;
+  final List notes;
 
   FolderModel({required this.name, required this.notes});
+
+  factory FolderModel.fromJson(Map<String, dynamic> json) {
+    return FolderModel(
+      name: json["name"],
+      notes: json["notes"],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      "name": name,
+      "notes": notes,
+    };
+  }
 }
