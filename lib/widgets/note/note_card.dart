0c50eda15d37610e83d77436460105e8a7a7b282
Ali Maddahi <alimaddahi99@gmail.com>
2022-08-12 18:09:10 +0430

    implement isFolder in note_card and get the folders notes count and show it on card

--- lib/widgets/note/note_card.dart
+++ lib/widgets/note/note_card.dart
@@ -1,20 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:notes_getx/controllers/app_controller.dart';
+import 'package:notes_getx/controllers/note/folder_controller.dart';
 import 'package:notes_getx/controllers/note/note_controller.dart';
 import 'package:notes_getx/screens/note/add_edit_note.dart';
 
 class NoteCard extends StatelessWidget {
   final int index;
   final bool highlighted;
+  final bool isFolder;
   NoteCard({
     Key? key,
     required this.index,
     this.highlighted = false,
+    this.isFolder = false,
   }) : super(key: key);
 
   final AppController _appController = Get.find();
   final NoteController _noteController = Get.find();
+  final FolderController _folderController = Get.find();
+
+  String getNoteCountInFolder() {
+    print("folder: ${_folderController.folders.map((folder) => folder.name)}");
+    var f = _folderController.folders.singleWhere(
+        (folder) => folder.name == _noteController.notes[index].folderName);
+    return f.notes.length.toString();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -50,9 +61,8 @@ class NoteCard extends StatelessWidget {
                 },
                 child: Padding(
                   padding: const EdgeInsets.all(16.0),
-                  child: _noteController.notes[index].folder != null
+                  child: isFolder
                       ? Row(
-                          textDirection: TextDirection.ltr,
                           children: [
                             const Icon(
                               Icons.folder_rounded,
@@ -70,26 +80,27 @@ class NoteCard extends StatelessWidget {
                                             CrossAxisAlignment.start,
                                         children: [
                                           Text(
+                                            _noteController
+                                                .notes[index].folderName,
                                             maxLines: 1,
                                             overflow: TextOverflow.ellipsis,
-                                            "${_noteController.notes[index].folder}",
                                             style: const TextStyle(
                                               fontSize: 18.0,
                                               fontWeight: FontWeight.bold,
                                             ),
                                           ),
-                                          const Text(
-                                            "0",
-                                            style: TextStyle(
+                                          Text(
+                                            getNoteCountInFolder(),
+                                            style: const TextStyle(
                                               color: Colors.grey,
                                             ),
                                           ),
                                         ],
                                       )
                                     : Text(
+                                        _noteController.notes[index].folderName,
                                         maxLines: 1,
                                         overflow: TextOverflow.ellipsis,
-                                        "${_noteController.notes[index].folder}",
                                         style: const TextStyle(
                                           fontSize: 18.0,
                                           fontWeight: FontWeight.bold,
@@ -102,9 +113,9 @@ class NoteCard extends StatelessWidget {
                                     padding: _appController.isSelectMode.value
                                         ? const EdgeInsets.only(right: 40.0)
                                         : EdgeInsets.zero,
-                                    child: const Text(
-                                      "0",
-                                      style: TextStyle(
+                                    child: Text(
+                                      getNoteCountInFolder(),
+                                      style: const TextStyle(
                                         color: Colors.grey,
                                       ),
                                     ),
