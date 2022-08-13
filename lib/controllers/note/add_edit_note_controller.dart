db8e739ac703042767ac874ea0de635721582bd6
Ali Maddahi <alimaddahi99@gmail.com>
2022-08-13 20:32:59 +0430

    make add_edit_note_controller dynamic to work in both folder_screen and note_home

--- lib/controllers/note/add_edit_note_controller.dart
+++ lib/controllers/note/add_edit_note_controller.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:notes_getx/controllers/note/folder_controller.dart';
 import 'package:notes_getx/controllers/note/note_controller.dart';
 import 'package:notes_getx/models/note_model.dart';
 
@@ -8,45 +9,96 @@ class AddEditNoteController extends GetxController {
   final TextEditingController noteTextController = TextEditingController();
   final FocusNode noteFocusNode = FocusNode();
 
-  var args = Get.arguments;
+  // var args = Get.arguments;
+  NoteModel? args = Get.arguments;
   var isSaveButtonVisible = false.obs;
 
   final NoteController _noteController = Get.find();
+  final FolderController _folderController = Get.find();
+
+  // void addNote({
+  //   required String title,
+  //   required String note,
+  //   String folder = "parent",
+  // }) {
+  //   if (title.isNotEmpty || note.isNotEmpty) {
+  //     _noteController.notes.insert(
+  //       0,
+  //       NoteModel(
+  //         id: DateTime.now().microsecondsSinceEpoch,
+  //         note: note,
+  //         title: title,
+  //         dateTime: dateTimeNowStringify(),
+  //         folderName: folder,
+  //         isFolder: false,
+  //       ),
+  //     );
+
+  //     // args is index of note in the list
+  //     // so make args = 0; make it possible to be editable after adding the note
+  //     // because the note is added to index 0
+  //      args = 0;
+  //   }
+  // }
 
   void addNote({
     required String title,
     required String note,
-    String folder = "parent",
+    required String folderName,
   }) {
     if (title.isNotEmpty || note.isNotEmpty) {
-      _noteController.notes.insert(
-        0,
-        NoteModel(
-          id: DateTime.now().microsecondsSinceEpoch,
-          note: note,
-          title: title,
-          dateTime: dateTimeNowStringify(),
-          folderName: folder,
-          isFolder: false,
-        ),
+      var noteModel = NoteModel(
+        id: DateTime.now().microsecondsSinceEpoch,
+        note: note,
+        title: title,
+        dateTime: dateTimeNowStringify(),
+        folderName: folderName,
+        isFolder: false,
       );
 
-      // args is index of note in the list
-      // so make args = 0; make it possible to be editable after adding the note
-      // because the note is added to index 0
-      args = 0;
+      if (folderName == "parent") {
+        _noteController.notes.insert(
+          0,
+          noteModel,
+        );
+      } else {
+        var folders = _folderController.folders.map((folder) => folder);
+        for (var f in folders) {
+          if (f.name == folderName) {
+            f.notes.insert(0, noteModel);
+          }
+        }
+      }
+
+      // args is the note in the list
+      // so make args = noteModel; make it possible to be editable
+      // after adding the note
+      // because args = null wold run addNote() again
+      args = noteModel;
     }
   }
 
+  // void editNote() {
+  //   if (titleTextController.text.isEmpty && noteTextController.text.isEmpty) {
+  //     _noteController.deleteNote(_noteController.notes[args].id);
+  //   } else {
+  //     var edited = _noteController.notes[args];
+  //     edited.title = titleTextController.text;
+  //     edited.note = noteTextController.text;
+  //     edited.dateTime = dateTimeNowStringify();
+  //     _noteController.notes[args] = edited;
+  //   }
+  // }
+
   void editNote() {
     if (titleTextController.text.isEmpty && noteTextController.text.isEmpty) {
-      _noteController.deleteNote(_noteController.notes[args].id);
+      _noteController.deleteNote(args!.id);
     } else {
-      var edited = _noteController.notes[args];
+      var edited = args!;
       edited.title = titleTextController.text;
       edited.note = noteTextController.text;
       edited.dateTime = dateTimeNowStringify();
-      _noteController.notes[args] = edited;
+      args = edited;
     }
   }
 
@@ -62,8 +114,12 @@ class AddEditNoteController extends GetxController {
   @override
   void onInit() {
     if (args != null) {
-      titleTextController.text = _noteController.notes[args].title;
-      noteTextController.text = _noteController.notes[args].note;
+      // titleTextController.text = _noteController.notes[args].title;
+      // noteTextController.text = _noteController.notes[args].note;
+
+      titleTextController.text = args!.title;
+      noteTextController.text = args!.note;
+
       isSaveButtonVisible.value = true;
     } else {
       noteFocusNode.requestFocus();
