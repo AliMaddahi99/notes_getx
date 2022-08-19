d8676eb193d09ce83a935275585c81bed215f6ce
Ali Maddahi <alimaddahi99@gmail.com>
2022-08-19 21:00:41 +0430

    add WidgetsFlutterBinding.ensureInitialized(); to main

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:get_storage/get_storage.dart';
-
 import 'notes_app.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await GetStorage.init();
   runApp(const NotesApp());
 }
