6c5adff89e59f966e3f5d23f23e5c90c74159e22
Ali Maddahi <alimaddahi99@gmail.com>
2022-08-24 00:10:46 +0430

    remove folderName and NoItem() from note_home

--- lib/screens/note/note_home.dart
+++ lib/screens/note/note_home.dart
@@ -1,29 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:notes_getx/controllers/app_controller.dart';
-import 'package:notes_getx/controllers/note/note_controller.dart';
 import 'package:notes_getx/screens/note/add_edit_note.dart';
-import 'package:notes_getx/widgets/no_item.dart';
 import 'package:notes_getx/widgets/note/view_note_card.dart';
 
 class NoteHome extends StatelessWidget {
   NoteHome({Key? key}) : super(key: key);
 
   final AppController _appController = Get.find();
-  final NoteController _noteController = Get.find();
 
   @override
   Widget build(BuildContext context) {
     return Obx(
       () => Scaffold(
-        body: _noteController.notes.isNotEmpty ? ViewNoteCard() : NoItem(),
+        body: ViewNoteCard(),
         floatingActionButton: !_appController.isSelectMode.value
             ? FloatingActionButton(
                 onPressed: () => {
                   Get.to(
-                    () => AddEditNote(
-                      folderName: "parent",
-                    ),
+                    () => AddEditNote(),
                     transition: Transition.cupertino,
                   ),
                 },
