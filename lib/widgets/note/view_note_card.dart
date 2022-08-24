f854e90fd091874c0a48aeb4859834e47ccfc85d
Ali Maddahi <alimaddahi99@gmail.com>
2022-08-24 16:56:14 +0430

    change the view_note_card to work with isar

--- lib/widgets/note/view_note_card.dart
+++ lib/widgets/note/view_note_card.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:get/get.dart';
+import 'package:isar/isar.dart';
 import 'package:notes_getx/controllers/app_controller.dart';
 import 'package:notes_getx/controllers/note/folder_controller.dart';
 import 'package:notes_getx/controllers/note/note_controller.dart';
+import 'package:notes_getx/models/note.dart';
+import 'package:notes_getx/widgets/no_item.dart';
 import 'package:notes_getx/widgets/note/folder_bottom_sheet.dart';
 import 'package:notes_getx/widgets/note/folder_card.dart';
 import 'package:notes_getx/widgets/note/note_card.dart';
@@ -12,8 +14,8 @@ import 'package:notes_getx/widgets/note/note_card.dart';
 class ViewNoteCard extends StatelessWidget {
   ViewNoteCard({Key? key}) : super(key: key);
 
-  final NoteController _noteController = Get.find();
   final AppController _appController = Get.find();
+  final NoteController _noteController = Get.find();
   final FolderController _folderController = Get.find();
 
   int gridCrossAxisCount(BuildContext context) {
@@ -29,118 +31,132 @@ class ViewNoteCard extends StatelessWidget {
     return count;
   }
 
+  Stream<List<Note>> getNotes() {
+    return _appController.db.notes
+        .where()
+        .filter()
+        .folderNameEqualTo("parent")
+        .or()
+        .isFolderEqualTo(true)
+        .build()
+        .watch(initialReturn: true);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Obx(
-      () => MasonryGridView.count(
-        cacheExtent: 500,
-        controller: _noteController.scrollController,
-        key: const PageStorageKey<String>("notesPage"),
-        padding: const EdgeInsets.all(8.0),
-        crossAxisCount: gridCrossAxisCount(context),
-        itemCount: _noteController.notes.length,
-        itemBuilder: (context, index) {
-          return AnimationLimiter(
-            child: AnimationConfiguration.staggeredGrid(
-              position: index,
-              duration: const Duration(milliseconds: 300),
-              columnCount: gridCrossAxisCount(context),
-              child: ScaleAnimation(
-                child: FadeInAnimation(
-                  child: DragTarget(
-                    builder: (context, candidateData, rejectedData) {
-                      return _noteController.notes[index].isFolder
-                          ? FolderCard(
-                              index: index,
-                              highlighted: candidateData.isNotEmpty,
-                            )
-                          : LongPressDraggable(
-                              onDragStarted: () {
-                                _appController.isSelectMode.value = true;
-                                _appController.selectItem(
-                                    _noteController.notes[index].id!);
-                              },
-                              onDragUpdate: (details) {
-                                _appController.selectedItems.clear();
-
-                                if (details.globalPosition.dy < 100) {
-                                  _noteController.scrollController.animateTo(
-                                    _noteController.scrollController.offset -
-                                        50,
-                                    duration: const Duration(milliseconds: 300),
-                                    curve: Curves.linear,
-                                  );
-                                } else if (details.globalPosition.dy >
-                                    MediaQuery.of(context).size.height - 100) {
-                                  _noteController.scrollController.animateTo(
-                                    _noteController.scrollController.offset +
-                                        50,
-                                    duration: const Duration(milliseconds: 300),
-                                    curve: Curves.linear,
-                                  );
-                                }
-                              },
-                              data: _noteController.notes[index].id,
-                              childWhenDragging: Opacity(
-                                opacity: 0.0,
-                                child: NoteCard(
-                                  index: index,
-                                  note: _noteController.notes[index],
-                                ),
-                              ),
-                              feedback: SizedBox(
-                                // using MediaQuery to set NoteCard width, otherwise,
-                                // it's width would be as long as NoteCard content
-                                width: _noteController.isGridView.value
-                                    ? MediaQuery.of(context).size.width / 2 - 8
-                                    : MediaQuery.of(context).size.width - 16,
-                                child: NoteCard(
-                                  index: index,
-                                  note: _noteController.notes[index],
-                                ),
-                              ),
-                              child: NoteCard(
-                                index: index,
-                                note: _noteController.notes[index],
+    return StreamBuilder<List<Note>>(
+      initialData: const [],
+      stream: getNotes(),
+      builder: (BuildContext context, AsyncSnapshot<List<Note>> snapshot) {
+        return snapshot.data!.isEmpty
+            ? NoItem()
+            : Obx(
+                () => MasonryGridView.count(
+                  cacheExtent: 500,
+                  controller: _noteController.scrollController,
+                  key: const PageStorageKey<String>("notesPage"),
+                  padding: const EdgeInsets.all(8.0),
+                  crossAxisCount: gridCrossAxisCount(context),
+                  itemCount: snapshot.data!.length,
+                  itemBuilder: (context, index) {
+                    // show the list in reverse order,
+                    // so the last item that is added,
+                    // placed at index 0 in MasonryGridView
+                    int reversedIndex = snapshot.data!.length - 1 - index;
+                    return DragTarget(
+                      builder: (context, candidateData, rejectedData) {
+                        return snapshot.data![reversedIndex].isFolder
+                            ? FolderCard(
+                                note: snapshot.data![reversedIndex],
                                 highlighted: candidateData.isNotEmpty,
-                              ),
-                            );
-                    },
-                    onWillAccept: (data) {
-                      return data != _noteController.notes[index].id;
-                    },
-                    onAccept: (data) {
-                      if (_noteController.notes[index].isFolder) {
-                        _folderController.addNoteToExistingFolder(
-                          _noteController.notes[index].folderName,
-                          _noteController.notes
-                              .firstWhere((note) => note.id == data),
-                        );
-                      } else {
-                        Get.bottomSheet(
-                          FolderBottomSheet(
-                            title: "New Folder",
-                            targetNoteId: _noteController.notes[index].id!,
-                            draggingNoteId: data as int,
-                          ),
-                          backgroundColor: Colors.white,
-                          shape: const RoundedRectangleBorder(
-                            borderRadius: BorderRadius.vertical(
-                              top: Radius.circular(20.0),
+                              )
+                            : LongPressDraggable(
+                                onDragStarted: () {
+                                  _appController.isSelectMode.value = true;
+                                  _appController.selectItem(
+                                      snapshot.data![reversedIndex].id);
+                                },
+                                onDragUpdate: (details) {
+                                  _appController.selectedItems.clear();
+
+                                  if (details.globalPosition.dy < 100) {
+                                    _noteController.scrollController.animateTo(
+                                      _noteController.scrollController.offset -
+                                          50,
+                                      duration:
+                                          const Duration(milliseconds: 300),
+                                      curve: Curves.linear,
+                                    );
+                                  } else if (details.globalPosition.dy >
+                                      MediaQuery.of(context).size.height -
+                                          100) {
+                                    _noteController.scrollController.animateTo(
+                                      _noteController.scrollController.offset +
+                                          50,
+                                      duration:
+                                          const Duration(milliseconds: 300),
+                                      curve: Curves.linear,
+                                    );
+                                  }
+                                },
+                                data: snapshot.data![reversedIndex],
+                                childWhenDragging: Opacity(
+                                  opacity: 0.0,
+                                  child: NoteCard(
+                                    note: snapshot.data![reversedIndex],
+                                  ),
+                                ),
+                                feedback: SizedBox(
+                                  // using MediaQuery to set NoteCard width, otherwise,
+                                  // it's width would be as long as NoteCard content
+                                  width: _noteController.isGridView.value
+                                      ? MediaQuery.of(context).size.width / 2 -
+                                          8
+                                      : MediaQuery.of(context).size.width - 16,
+                                  child: NoteCard(
+                                    note: snapshot.data![reversedIndex],
+                                  ),
+                                ),
+                                child: NoteCard(
+                                  note: snapshot.data![reversedIndex],
+                                  highlighted: candidateData.isNotEmpty,
+                                ),
+                              );
+                      },
+                      onWillAccept: (data) {
+                        return (data as Note).id !=
+                            snapshot.data![reversedIndex].id;
+                      },
+                      onAccept: (data) {
+                        if (snapshot.data![reversedIndex].isFolder) {
+                          _folderController.addNoteToExistingFolder(
+                            snapshot.data![reversedIndex].folderName,
+                            data as Note,
+                          );
+                          _appController.isSelectMode.value = false;
+                        } else {
+                          Get.bottomSheet(
+                            FolderBottomSheet(
+                              title: "New Folder",
+                              targetNote: snapshot.data![reversedIndex],
+                              draggingNote: data as Note,
                             ),
-                          ),
-                          enableDrag: false,
-                          isDismissible: false,
-                        );
-                      }
-                    },
-                  ),
+                            backgroundColor: Colors.white,
+                            shape: const RoundedRectangleBorder(
+                              borderRadius: BorderRadius.vertical(
+                                top: Radius.circular(20.0),
+                              ),
+                            ),
+                            enableDrag: false,
+                            isDismissible: false,
+                          );
+                        }
+                      },
+                    );
+                  },
                 ),
-              ),
-            ),
-          );
-        },
-      ),
+              );
+      },
     );
   }
 }
