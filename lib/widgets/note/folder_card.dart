d184826fd0b935d781534860b16a72666c5eae81
Ali Maddahi <alimaddahi99@gmail.com>
2022-08-24 16:55:43 +0430

    change the folder_card to work with isar

--- lib/widgets/note/folder_card.dart
+++ lib/widgets/note/folder_card.dart
@@ -1,85 +1,111 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:isar/isar.dart';
 import 'package:notes_getx/controllers/app_controller.dart';
-import 'package:notes_getx/controllers/note/folder_controller.dart';
 import 'package:notes_getx/controllers/note/note_controller.dart';
+import 'package:notes_getx/models/note.dart';
 import 'package:notes_getx/screens/note/folder_screen.dart';
 
 class FolderCard extends StatelessWidget {
-  final int index;
+  final Note note;
   final bool highlighted;
   FolderCard({
     Key? key,
-    required this.index,
+    required this.note,
     this.highlighted = false,
   }) : super(key: key);
 
   final AppController _appController = Get.find();
   final NoteController _noteController = Get.find();
-  final FolderController _folderController = Get.find();
 
-  String getNoteCountInFolder() {
-    var f = _folderController.folders.singleWhere(
-        (folder) => folder.name == _noteController.notes[index].folderName);
-    return f.notes.length.toString();
+  Stream<List<Note>> getNotesInFolder() {
+    return _appController.db.notes
+        .where()
+        .filter()
+        .folderNameEqualTo(note.folderName)
+        .and()
+        .isFolderEqualTo(false)
+        .watch(initialReturn: true);
   }
 
   @override
   Widget build(BuildContext context) {
     return Material(
       color: Colors.transparent,
-      child: Stack(
-        alignment: _noteController.isGridView.value
-            ? Alignment.bottomRight
-            : Alignment.centerRight,
-        children: [
-          Card(
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(16.0),
-              side: BorderSide(
-                color: highlighted ? Colors.amber : Colors.transparent,
-              ),
-            ),
-            child: Obx(
-              () => InkWell(
-                mouseCursor: SystemMouseCursors.click,
-                borderRadius: BorderRadius.circular(16.0),
-                onTap: () {
-                  if (_appController.isSelectMode.value) {
-                    _appController.selectItem(_noteController.notes[index].id!);
-                  } else {
-                    Get.to(
-                      () => FolderScreen(
-                        folderName: _noteController.notes[index].folderName,
-                      ),
-                      transition: Transition.cupertino,
-                    );
-                  }
-                },
-                onLongPress: () {
-                  _appController.isSelectMode.value = true;
-                  _appController.selectItem(_noteController.notes[index].id!);
-                },
-                child: Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Row(
-                    children: [
-                      const Icon(
-                        Icons.folder_rounded,
-                        color: Colors.amber,
-                        size: 32.0,
-                      ),
-                      Expanded(
-                        child: Padding(
-                          padding: const EdgeInsets.symmetric(
-                            horizontal: 16.0,
+      child: StreamBuilder<List<Note>>(
+        initialData: const [],
+        stream: getNotesInFolder(),
+        builder: (BuildContext context, AsyncSnapshot<List<Note>> snapshot) {
+          return Stack(
+            alignment: _noteController.isGridView.value
+                ? Alignment.bottomRight
+                : Alignment.centerRight,
+            children: [
+              Card(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(16.0),
+                  side: BorderSide(
+                    color: highlighted ? Colors.amber : Colors.transparent,
+                  ),
+                ),
+                child: Obx(
+                  () => InkWell(
+                    mouseCursor: SystemMouseCursors.click,
+                    borderRadius: BorderRadius.circular(16.0),
+                    onTap: () {
+                      if (_appController.isSelectMode.value) {
+                        _appController.selectItem(note.id);
+                      } else {
+                        Get.to(
+                          () => FolderScreen(
+                            folderName: note.folderName,
                           ),
-                          child: _noteController.isGridView.value
-                              ? Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Text(
-                                      _noteController.notes[index].folderName,
+                          transition: Transition.cupertino,
+                        );
+                      }
+                    },
+                    onLongPress: () {
+                      _appController.isSelectMode.value = true;
+                      _appController.selectItem(note.id);
+                    },
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Row(
+                        children: [
+                          const Icon(
+                            Icons.folder_rounded,
+                            color: Colors.amber,
+                            size: 32.0,
+                          ),
+                          Expanded(
+                            child: Padding(
+                              padding: const EdgeInsets.symmetric(
+                                horizontal: 16.0,
+                              ),
+                              child: _noteController.isGridView.value
+                                  ? Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        Text(
+                                          note.folderName,
+                                          maxLines: 1,
+                                          overflow: TextOverflow.ellipsis,
+                                          style: const TextStyle(
+                                            fontSize: 18.0,
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                        ),
+                                        Text(
+                                          snapshot.data!.length.toString(),
+                                          style: const TextStyle(
+                                            color: Colors.grey,
+                                          ),
+                                        ),
+                                      ],
+                                    )
+                                  : Text(
+                                      note.folderName,
                                       maxLines: 1,
                                       overflow: TextOverflow.ellipsis,
                                       style: const TextStyle(
@@ -87,71 +113,54 @@ class FolderCard extends StatelessWidget {
                                         fontWeight: FontWeight.bold,
                                       ),
                                     ),
-                                    Text(
-                                      getNoteCountInFolder(),
-                                      style: const TextStyle(
-                                        color: Colors.grey,
-                                      ),
+                            ),
+                          ),
+                          !_noteController.isGridView.value
+                              ? Padding(
+                                  padding: _appController.isSelectMode.value
+                                      ? const EdgeInsets.only(right: 40.0)
+                                      : EdgeInsets.zero,
+                                  child: Text(
+                                    snapshot.data!.length.toString(),
+                                    style: const TextStyle(
+                                      color: Colors.grey,
                                     ),
-                                  ],
-                                )
-                              : Text(
-                                  _noteController.notes[index].folderName,
-                                  maxLines: 1,
-                                  overflow: TextOverflow.ellipsis,
-                                  style: const TextStyle(
-                                    fontSize: 18.0,
-                                    fontWeight: FontWeight.bold,
                                   ),
-                                ),
-                        ),
+                                )
+                              : const SizedBox.shrink(),
+                        ],
                       ),
-                      !_noteController.isGridView.value
-                          ? Padding(
-                              padding: _appController.isSelectMode.value
-                                  ? const EdgeInsets.only(right: 40.0)
-                                  : EdgeInsets.zero,
-                              child: Text(
-                                getNoteCountInFolder(),
-                                style: const TextStyle(
-                                  color: Colors.grey,
-                                ),
-                              ),
-                            )
-                          : const SizedBox.shrink(),
-                    ],
-                  ),
-                ),
-              ),
-            ),
-          ),
-          Obx(
-            () => Visibility(
-              visible: _appController.isSelectMode.value,
-              child: Transform.scale(
-                scale: 1.3,
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 10.0,
-                    vertical: 5.0,
-                  ),
-                  child: Checkbox(
-                    splashRadius: 0,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20.0),
                     ),
-                    value: _appController.selectedItems
-                        .contains(_noteController.notes[index].id),
-                    onChanged: (checked) {
-                      _appController
-                          .selectItem(_noteController.notes[index].id!);
-                    },
                   ),
                 ),
               ),
-            ),
-          ),
-        ],
+              Obx(
+                () => Visibility(
+                  visible: _appController.isSelectMode.value,
+                  child: Transform.scale(
+                    scale: 1.3,
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 10.0,
+                        vertical: 5.0,
+                      ),
+                      child: Checkbox(
+                        splashRadius: 0,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20.0),
+                        ),
+                        value: _appController.selectedItems.contains(note.id),
+                        onChanged: (checked) {
+                          _appController.selectItem(note.id);
+                        },
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          );
+        },
       ),
     );
   }
