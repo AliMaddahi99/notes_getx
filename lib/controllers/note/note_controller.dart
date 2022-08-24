001961eb721af6f75797cb4ac65db9dc6040e2bc
Ali Maddahi <alimaddahi99@gmail.com>
2022-08-24 16:52:57 +0430

    change the note_controller to work with isar

--- lib/controllers/note/note_controller.dart
+++ lib/controllers/note/note_controller.dart
@@ -1,29 +1,14 @@
 import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
-import 'package:notes_getx/models/note.dart';
 
 class NoteController extends GetxController {
-  var notes = <Note>[].obs;
-  var isGridView = false.obs;
-
   ScrollController scrollController = ScrollController();
 
-  void deleteNote(int id) {
-    notes.removeWhere((note) => note.id == id);
-  }
+  var isGridView = false.obs;
 
   @override
   void onInit() {
-    // Save and read notes
-    var storedNotes = GetStorage().read<List>("notes");
-    if (storedNotes != null) {
-      notes = storedNotes.map((e) => Note.fromMap(e)).toList().obs;
-    }
-    ever(notes, (_) {
-      GetStorage().write("notes", notes.toList());
-    });
-
     // Save and read Layout mode, Gridview/Listview
     var layoutIsGridView = GetStorage().read("layout");
     if (layoutIsGridView != null) {
