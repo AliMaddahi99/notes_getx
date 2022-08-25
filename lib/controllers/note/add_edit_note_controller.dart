b8ec5b16244a489a01e3af55ff7b0d84ccb5c482
Ali Maddahi <alimaddahi99@gmail.com>
2022-08-25 19:20:20 +0430

    remove useless comments from add_edit_note_controller

--- lib/controllers/note/add_edit_note_controller.dart
+++ lib/controllers/note/add_edit_note_controller.dart
@@ -12,55 +12,6 @@ class AddEditNoteController extends GetxController {
   Note? args = Get.arguments;
   var isSaveButtonVisible = false.obs;
 
-  // void addNote({
-  //   required String title,
-  //   required String note,
-  //   required String folderName,
-  // }) {
-  //   if (title.isNotEmpty || note.isNotEmpty) {
-  //     var noteModel = Note(
-  //       id: DateTime.now().microsecondsSinceEpoch,
-  //       note: note,
-  //       title: title,
-  //       dateTime: dateTimeNowStringify(),
-  //       folderName: folderName,
-  //       isFolder: false,
-  //     );
-
-  //     if (folderName == "parent") {
-  //       _noteController.notes.insert(
-  //         0,
-  //         noteModel,
-  //       );
-  //     } else {
-  //       var folders = _folderController.folders.map((folder) => folder);
-  //       for (var f in folders) {
-  //         if (f.name == folderName) {
-  //           f.notes.insert(0, noteModel);
-  //         }
-  //       }
-  //     }
-
-  //     // args is the note in the list
-  //     // so make args = noteModel; make it possible to be editable
-  //     // after adding the note
-  //     // because args = null wold run addNote() again
-  //     args = noteModel;
-  //   }
-  // }
-
-  // void editNote() {
-  //   if (titleTextController.text.isEmpty && noteTextController.text.isEmpty) {
-  //     _noteController.deleteNote(_noteController.notes[args].id);
-  //   } else {
-  //     var edited = _noteController.notes[args];
-  //     edited.title = titleTextController.text;
-  //     edited.note = noteTextController.text;
-  //     edited.dateTime = dateTimeNowStringify();
-  //     _noteController.notes[args] = edited;
-  //   }
-  // }
-
   Future<void> addNote({
     required String title,
     required String note,
