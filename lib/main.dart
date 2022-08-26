9a254aca7abb260f0bf2f197bd43f14ddfbd2302
Ali Maddahi <alimaddahi99@gmail.com>
2022-08-26 19:30:33 +0430

    create database and initialize isar in there and use it in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
-import 'package:isar/isar.dart';
-import 'package:notes_getx/controllers/app_controller.dart';
-import 'package:notes_getx/models/folder.dart';
-import 'package:notes_getx/models/note.dart';
-import 'package:path_provider/path_provider.dart';
+import 'package:notes_getx/services/database/database.dart';
 import 'notes_app.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  final AppController appController = Get.put(AppController());
-
   await GetStorage.init();
-
-  appController.db = await Isar.open(
-    schemas: [
-      NoteSchema,
-      FolderSchema,
-    ],
-    directory: (await getApplicationDocumentsDirectory()).path,
-  );
+  await Database().initIsar();
   // await appController.db.writeTxn((isar) async => await isar.notes.clear());
   // await appController.db.writeTxn((isar) async => await isar.folders.clear());
 
---
+++ lib/services/database/database.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:isar/isar.dart';
+import 'package:notes_getx/controllers/app_controller.dart';
+import 'package:notes_getx/models/folder.dart';
+import 'package:notes_getx/models/note.dart';
+import 'package:path_provider/path_provider.dart';
+
+class Database {
+  final AppController _appController = Get.put(AppController());
+
+  Future<Isar> initIsar() async {
+    WidgetsFlutterBinding.ensureInitialized();
+
+    return _appController.db = await Isar.open(
+      schemas: [
+        NoteSchema,
+        FolderSchema,
+      ],
+      directory: (await getApplicationDocumentsDirectory()).path,
+    );
+  }
+}
