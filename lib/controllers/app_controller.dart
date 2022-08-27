3767b441648fe82ffdc5d7f84bf1dfa1c279c377
Ali Maddahi <alimaddahi99@gmail.com>
2022-08-28 01:40:03 +0430

    add selectedFolderNotes selectFolderNotes() to app_controller

--- lib/controllers/app_controller.dart
+++ lib/controllers/app_controller.dart
@@ -4,15 +4,27 @@ import 'package:isar/isar.dart';
 
 class AppController extends GetxController {
   PageController pageController = PageController();
+  late Isar db;
   var pageViewId = 0.obs;
   var isSelectMode = false.obs;
   var selectedItems = <int>[].obs;
-  late Isar db;
+
+  // while 1 folder selected, store chlidren notes of that folder(s)
+  // in this variable, when delete button clicked,
+  // delete folder from selectedItems[],
+  // and delete children from selectedFolderNotes[]
+  var selectedFolderNotes = <int>[].obs;
 
   void selectItem(int id) {
     selectedItems.contains(id)
         ? selectedItems.remove(id)
         : selectedItems.add(id);
+  }
+
+  void selectFolderNotes(int id) {
+    selectedFolderNotes.contains(id)
+        ? selectedFolderNotes.remove(id)
+        : selectedFolderNotes.add(id);
   }
 
   String getSelectedItemsCount(String message) {
