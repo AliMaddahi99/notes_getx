fbcb829c90fc1b817848b8989bf37ba042dc1140
Ali Maddahi <alimaddahi99@gmail.com>
2022-09-02 17:05:24 +0430

    create delete_icon_button to use in select_mode_bottom_navigation_bar

---
+++ lib/widgets/delete_icon_button.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:notes_getx/controllers/app_controller.dart';
+import 'package:notes_getx/widgets/delete_bottom_sheet.dart';
+
+class DeleteIconButton extends StatelessWidget {
+  final String? folderName;
+  final bool deleteFromFolderScreen;
+  DeleteIconButton({
+    super.key,
+    this.folderName,
+    this.deleteFromFolderScreen = false,
+  });
+
+  final AppController _appController = Get.find();
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      onPressed: _appController.selectedItems.isNotEmpty
+          ? () {
+              Get.bottomSheet(
+                DeleteBottomSheet(
+                  title: _appController.pageViewId.value == 0
+                      ? "Delete note"
+                      : "Delete task",
+                  message:
+                      "Delete ${_appController.getSelectedItemsCount(_appController.pageViewId.value == 0 ? "item" : "task")}?",
+                  deleteFromFolderScreen: deleteFromFolderScreen,
+                  folderName: folderName,
+                ),
+                backgroundColor: Colors.white,
+                shape: const RoundedRectangleBorder(
+                  borderRadius: BorderRadius.vertical(
+                    top: Radius.circular(20.0),
+                  ),
+                ),
+              );
+            }
+          : null,
+      icon: const Icon(Icons.delete_rounded),
+    );
+  }
+}
