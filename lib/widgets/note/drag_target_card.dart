ddaefe4c02abcab7a4dd5e642e4275ae0e61acc4
Ali Maddahi <alimaddahi99@gmail.com>
2022-09-04 02:53:51 +0430

    create drag_target_card and use it in view_note_card

---
+++ lib/widgets/note/drag_target_card.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:notes_getx/controllers/app_controller.dart';
+import 'package:notes_getx/controllers/note/folder_controller.dart';
+import 'package:notes_getx/models/note.dart';
+import 'package:notes_getx/widgets/note/folder/folder_bottom_sheet.dart';
+import 'package:notes_getx/widgets/note/folder/folder_card.dart';
+import 'package:notes_getx/widgets/note/long_press_draggable_note_card.dart';
+
+class DragTargetCard extends StatelessWidget {
+  final Note note;
+
+  DragTargetCard({super.key, required this.note});
+
+  final AppController _appController = Get.find();
+  final FolderController _folderController = Get.find();
+
+  @override
+  Widget build(BuildContext context) {
+    return DragTarget(
+      builder: (context, candidateData, rejectedData) {
+        return note.isFolder
+            ? FolderCard(
+                note: note,
+                highlighted: candidateData.isNotEmpty,
+              )
+            : LongPressDraggableNoteCard(
+                note: note,
+                highlighted: candidateData.isNotEmpty,
+              );
+      },
+      onWillAccept: (data) {
+        return (data as Note).id != note.id;
+      },
+      onAccept: (data) {
+        if (note.isFolder) {
+          _folderController.addNoteToExistingFolder(
+            note.folderName!,
+            data as Note,
+          );
+          _appController.isSelectMode.value = false;
+        } else {
+          Get.bottomSheet(
+            FolderBottomSheet(
+              title: "New Folder",
+              targetNote: note,
+              draggingNote: data as Note,
+            ),
+            enableDrag: false,
+            isDismissible: false,
+          );
+        }
+      },
+    );
+  }
+}
--- lib/widgets/note/view_note_card.dart
+++ lib/widgets/note/view_note_card.dart
@@ -3,20 +3,16 @@ import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:get/get.dart';
 import 'package:isar/isar.dart';
 import 'package:notes_getx/controllers/app_controller.dart';
-import 'package:notes_getx/controllers/note/folder_controller.dart';
 import 'package:notes_getx/controllers/note/note_controller.dart';
 import 'package:notes_getx/models/note.dart';
 import 'package:notes_getx/widgets/app/no_item.dart';
-import 'package:notes_getx/widgets/note/folder/folder_bottom_sheet.dart';
-import 'package:notes_getx/widgets/note/folder/folder_card.dart';
-import 'package:notes_getx/widgets/note/long_press_draggable_note_card.dart';
+import 'package:notes_getx/widgets/note/drag_target_card.dart';
 
 class ViewNoteCard extends StatelessWidget {
   ViewNoteCard({Key? key}) : super(key: key);
 
   final AppController _appController = Get.find();
   final NoteController _noteController = Get.find();
-  final FolderController _folderController = Get.find();
 
   int gridCrossAxisCount(BuildContext context) {
     var width = MediaQuery.of(context).size.width;
@@ -63,42 +59,8 @@ class ViewNoteCard extends StatelessWidget {
                     // so the last item that is added,
                     // placed at index 0 in MasonryGridView
                     int reversedIndex = snapshot.data!.length - 1 - index;
-                    return DragTarget(
-                      builder: (context, candidateData, rejectedData) {
-                        return snapshot.data![reversedIndex].isFolder
-                            ? FolderCard(
-                                note: snapshot.data![reversedIndex],
-                                highlighted: candidateData.isNotEmpty,
-                              )
-                            : LongPressDraggableNoteCard(
-                                note: snapshot.data![reversedIndex],
-                                highlighted: candidateData.isNotEmpty,
-                              );
-                      },
-                      onWillAccept: (data) {
-                        return (data as Note).id !=
-                            snapshot.data![reversedIndex].id;
-                      },
-                      onAccept: (data) {
-                        if (snapshot.data![reversedIndex].isFolder) {
-                          _folderController.addNoteToExistingFolder(
-                            snapshot.data![reversedIndex].folderName!,
-                            data as Note,
-                          );
-                          _appController.isSelectMode.value = false;
-                        } else {
-                          Get.bottomSheet(
-                            FolderBottomSheet(
-                              title: "New Folder",
-                              targetNote: snapshot.data![reversedIndex],
-                              draggingNote: data as Note,
-                            ),
-                            enableDrag: false,
-                            isDismissible: false,
-                          );
-                        }
-                      },
-                    );
+
+                    return DragTargetCard(note: snapshot.data![reversedIndex]);
                   },
                 ),
               );
