6669edefcb5fde640ae502e1c76d697ddf17cfd7
Ali Maddahi <alimaddahi99@gmail.com>
2022-09-04 04:13:52 +0430

    move view_note_card code to note_home and  delete view_note_card

--- lib/screens/note/note_home.dart
+++ lib/screens/note/note_home.dart
@@ -1,16 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:isar/isar.dart';
+import 'package:notes_getx/controllers/app_controller.dart';
+import 'package:notes_getx/models/note.dart';
 import 'package:notes_getx/screens/note/add_edit_note.dart';
 import 'package:notes_getx/widgets/app/add_fab.dart';
-import 'package:notes_getx/widgets/note/view_note_card.dart';
+import 'package:notes_getx/widgets/note/stream_builder_masonry_grid_view.dart';
 
 class NoteHome extends StatelessWidget {
-  const NoteHome({Key? key}) : super(key: key);
+  NoteHome({Key? key}) : super(key: key);
+
+  final AppController _appController = Get.find();
+
+  Stream<List<Note>> getNotes() {
+    return _appController.db.notes
+        .where()
+        .filter()
+        .folderNameIsNull()
+        .or()
+        .isFolderEqualTo(true)
+        .build()
+        .watch(initialReturn: true);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ViewNoteCard(),
+      body: StreamBuilderMasonryGridView(
+        stream: getNotes(),
+        returnCard: ReturnCard.dragTargetCard,
+        isViewNoteCard: true,
+      ),
       floatingActionButton: AddFAB(
         onPressed: () => {
           Get.to(
