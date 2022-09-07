1e70ef5e54cd9d35efa5af331b90cd6ed8ce08ce
Ali Maddahi <alimaddahi99@gmail.com>
2022-09-07 15:56:36 +0430

    implement rename_button functionality

--- lib/widgets/app/select_mode_bottom_bar/rename_button.dart
+++ lib/widgets/app/select_mode_bottom_bar/rename_button.dart
@@ -1,25 +1,50 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:isar/isar.dart';
 import 'package:notes_getx/controllers/app_controller.dart';
+import 'package:notes_getx/models/note.dart';
+import 'package:notes_getx/services/database/note_database_service.dart';
 import 'package:notes_getx/widgets/app/select_mode_bottom_bar/select_mode_bottom_bar_button.dart';
+import 'package:notes_getx/widgets/note/folder/folder_bottom_sheet.dart';
 
 class RenameButton extends StatelessWidget {
-  final String? folderName;
-  final bool deleteFromFolderScreen;
-  RenameButton({
-    super.key,
-    this.folderName,
-    this.deleteFromFolderScreen = false,
-  });
+  RenameButton({super.key});
 
   final AppController _appController = Get.find();
 
+  Future<List<Note>> getNotesInFolder() async {
+    var n = await NoteDatabaseService()
+        .getNoteFromDb(_appController.selectedItems.first);
+
+    return _appController.db.notes
+        .where()
+        .filter()
+        .folderNameEqualTo(n!.folderName)
+        .findAll();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return SelectModeBottomBarButton(
-      onPressed: () {},
-      icon: Icons.drive_file_rename_outline_rounded,
-      lable: "Rename",
+    return FutureBuilder<List<Note>>(
+      initialData: const [],
+      future: getNotesInFolder(),
+      builder: (BuildContext context, AsyncSnapshot<List<Note>> snapshot) {
+        return SelectModeBottomBarButton(
+          onPressed: () {
+            Get.bottomSheet(
+              FolderBottomSheet(
+                title: "Rename folder",
+                notes: snapshot.data!,
+                isRename: true,
+              ),
+              enableDrag: false,
+              isDismissible: false,
+            );
+          },
+          icon: Icons.drive_file_rename_outline_rounded,
+          lable: "Rename",
+        );
+      },
     );
   }
 }
