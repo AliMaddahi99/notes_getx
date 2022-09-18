ea86ac0f837bf209cb2d09c3147a191c2722707a
Ali Maddahi <alimaddahi99@gmail.com>
2022-09-18 21:33:37 +0430

    transparent ReorderableListView background in reorderable_task_card_listView

--- lib/widgets/task/reorderable_task_card_listview.dart
+++ lib/widgets/task/reorderable_task_card_listview.dart
@@ -13,29 +13,35 @@ class ReorderableTaskCardListView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Obx(
-      () => ReorderableListView.builder(
-        key: const PageStorageKey<String>("tasksPage"),
-        onReorder: (oldIndex, newIndex) {
-          if (oldIndex < newIndex) {
-            newIndex -= 1;
-          }
-          var item = _taskController.tasks.removeAt(oldIndex);
-          _taskController.tasks.insert(newIndex, item);
-        },
-        onReorderStart: (index) {
-          _appController.isSelectMode.value = true;
-          if (_appController.selectedItems.isEmpty) {
-            _appController.selectItem(_taskController.tasks[index].id);
-          }
-        },
-        padding: const EdgeInsets.all(8.0),
-        itemCount: _taskController.tasks.length,
-        itemBuilder: (context, index) {
-          return DismissibleTaskItemCard(
-            key: Key(_taskController.tasks[index].id.toString()),
-            index: index,
-          );
-        },
+      () => Theme(
+        data: Theme.of(context).copyWith(
+          canvasColor: Colors.transparent,
+          shadowColor: Colors.white,
+        ),
+        child: ReorderableListView.builder(
+          key: const PageStorageKey<String>("tasksPage"),
+          onReorder: (oldIndex, newIndex) {
+            if (oldIndex < newIndex) {
+              newIndex -= 1;
+            }
+            var item = _taskController.tasks.removeAt(oldIndex);
+            _taskController.tasks.insert(newIndex, item);
+          },
+          onReorderStart: (index) {
+            _appController.isSelectMode.value = true;
+            if (_appController.selectedItems.isEmpty) {
+              _appController.selectItem(_taskController.tasks[index].id);
+            }
+          },
+          padding: const EdgeInsets.all(8.0),
+          itemCount: _taskController.tasks.length,
+          itemBuilder: (context, index) {
+            return DismissibleTaskItemCard(
+              key: Key(_taskController.tasks[index].id.toString()),
+              index: index,
+            );
+          },
+        ),
       ),
     );
   }
